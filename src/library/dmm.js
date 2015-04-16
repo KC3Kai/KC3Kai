d8c3e11e2608651bc636822dfd064183d0ab2fab
Jet Rico <jetriconew@gmail.com>
2015-04-16 18:31:58 +0900

    DMM CSS Override Clean-up

--- src/library/dmm.js
+++ src/library/dmm.js
@@ -7,18 +7,12 @@ chrome.runtime.sendMessage({
 	
 	// if yes, apply CSS overrides
 	if(response.value){
-		$("body").css("margin", "0");
+		$("body").css("margin", "0px");
 		$("body").css("overflow", "hidden");
-		
 		$("#game_frame").css("position", "fixed");
-		$("#game_frame").css("left", "50%");
+		$("#game_frame").css("left", "-50px");
 		$("#game_frame").css("top", "-16px");
-		$("#game_frame").css("margin-left", "-450px");
 		$("#game_frame").css("z-index", "1");
-		
-		$("#flashWrap").css("position", "free");
-		$("#flashWrap").css("z-index", "1");
-		$("#flashWrap").css("margin", "0");
 	}
 	
 });
