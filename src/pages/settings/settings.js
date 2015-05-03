cf0798e87ab68747606f21b044ee08b666ce1164
Jet Rico <jetriconew@gmail.com>
2015-05-03 12:16:49 +0900

    Settings Page update

--- src/pages/settings/settings.js
+++ src/pages/settings/settings.js
@@ -9,8 +9,143 @@ $(document).on("ready", function(){
 	// Get current configs
 	app.Config.init();
 	
-	console.log(app.Config);
-	
+	// Show version number
 	$(".about_version span").text(manifest.version);
+	$(".kc3kaiextid").text(chrome.runtime.id);
+	
+	// On-Screen Quest Translations
+	showInput_tloverlay();
+	$(".tloverlay").on("click", function(){
+		app.Config.tl_overlay = !app.Config.tl_overlay;
+		app.Config.save();
+		showInput_tloverlay();
+	});
+	
+	// Timer allowance
+	showInput_allowance();
+	$(".allowance input").on("change", function(){
+		app.Config.time_dev = parseInt($(this).val(), 10) || 0;
+		app.Config.save();
+		showInput_allowance();
+	});
+	
+	// Alert Sounds
+	showInput_alerting();
+	$(".alerting .option").on("click", function(){
+		app.Config.timerAlert = $(this).data("num");
+		app.Config.save();
+		showInput_alerting();
+	});
+	
+	// Alert Volume
+	showInput_volume();
+	$(".volume input").on("change", function(){
+		app.Config.alert_volume = parseInt($(this).val(), 10) || 0;
+		app.Config.save();
+		showInput_volume();
+	});
+	
+	// Desktop Notification
+	showInput_desktop();
+	$(".desktop").on("click", function(){
+		app.Config.desktop_notif = !app.Config.desktop_notif;
+		app.Config.save();
+		showInput_desktop();
+	});
+	
+	// Screenshot Mode
+	showInput_ssmode();
+	$(".ssmode .option").on("click", function(){
+		app.Config.ss_mode = $(this).data("num");
+		app.Config.save();
+		showInput_ssmode();
+	});
+	
+	// Game Screen: Top Margin
+	showInput_margin();
+	$(".margin input").on("change", function(){
+		app.Config.gambox_margin = parseInt($(this).val(), 10) || 0;
+		app.Config.save();
+		showInput_margin();
+	});
+	
+	// Game Screen: Background
+	showInput_background();
+	$(".background input").on("change", function(){
+		app.Config.background = $(this).val();
+		app.Config.save();
+		showInput_background();
+	});
+	
+	// Reveal ship face
+	showInput_reveal();
+	$(".reveal").on("click", function(){
+		app.Config.reveal_names = !app.Config.reveal_names;
+		app.Config.save();
+		showInput_reveal();
+	});
 	
 });
+
+function showInput_tloverlay(){
+	$(".tloverlay").fadeOut(200, function(){
+		$("input", this).prop("checked", app.Config.tl_overlay);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_allowance(){
+	$(".allowance").fadeOut(200, function(){
+		$("input", this).val(app.Config.time_dev);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_alerting(){
+	$(".alerting").fadeOut(200, function(){
+		$(".option-"+app.Config.timerAlert+" input", this).prop("checked", true);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_volume(){
+	$(".volume").fadeOut(200, function(){
+		$("input", this).val(app.Config.alert_volume);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_desktop(){
+	$(".desktop").fadeOut(200, function(){
+		$("input", this).prop("checked", app.Config.desktop_notif);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_ssmode(){
+	$(".ssmode").fadeOut(200, function(){
+		$(".option-"+app.Config.ss_mode+" input", this).prop("checked", true);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_margin(){
+	$(".margin").fadeOut(200, function(){
+		$("input", this).val(app.Config.gambox_margin);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_background(){
+	$(".background").fadeOut(200, function(){
+		$("input", this).val(app.Config.background);
+		$(this).fadeIn(200);
+	});
+}
+
+function showInput_reveal(){
+	$(".reveal").fadeOut(200, function(){
+		$("input", this).prop("checked", app.Config.reveal_names);
+		$(this).fadeIn(200);
+	});
+}
