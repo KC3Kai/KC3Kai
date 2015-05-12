450e66145a546481b2f5bc1c93a0aee583e769ba
Jet Rico <jetriconew@gmail.com>
2015-05-12 21:10:57 +0900

    Strategy Room Implementation Refactor

---
+++ src/pages/strategy/classes/Strategy.js
@@ -0,0 +1,87 @@
+KC3.prototype.Strategy  = {
+	
+	// Collection of PageTab Wrapper Classes
+	tabs: {
+		"home": TabHome,
+		"fleets": TabFleets,
+		"ships": TabShips,
+		"items": TabItems,
+		"sortie": TabSortie,
+		"exops": TabExops,
+		"e_spring2015": TabE_spring2015,
+		"devmt": TabDevmt,
+		"crafts": TabCrafts,
+		"lscs": TabLscs,
+		"rscs": TabResources,
+		"consumes": TabConsumes
+	},
+	
+	// Tab currently being loaded
+	openingTab: "",
+	
+	// Page ready
+	ready: function(){
+		var self = this;
+		
+		// Navigation Link onClicks
+		$(".nav.nav-link").on("click", function(){
+			// If already in the process of opening another tab
+			if(self.openingTab != ""){ return false; }
+			
+			// Set "opening this tab" flag
+			self.openingTab = $(this).data("id");
+			
+			// Empty page contents
+			$(".tabs-content .tab-html").hide();
+			$(".tabs-content .tab-html").html("");
+			$(".tabs-content .tab-message").hide();
+			
+			// Highlight selected tab
+			$(".nav.nav-link").removeClass("active");
+			$(this).addClass("active");
+			
+			// Add page hash
+			window.location = "#"+self.openingTab;
+			
+			// Request the contents of selected tab and execute
+			$.ajax({
+				url: "html/"+self.openingTab+".html",
+				success: function(response){
+					// Show tab HTML
+					$(".tabs-content .tab-html").html(response);
+					$(".tabs-content .tab-html").show();
+					
+					// Execute tab scripts
+					self.tabs[ self.openingTab ].init();
+					self.tabs[ self.openingTab ].show();
+					
+					// Reset "opening this tab" flag
+					self.openingTab = "";
+				},
+				error: function(){
+					// Reset "opening this tab" flag
+					self.openingTab = "";
+				}
+			});
+		});
+		
+		// If there is a hash tag on URL, set it as initial selected
+		if(window.location.hash != ""){
+			$(".nav.nav-link").removeClass("active");
+			$(".nav.nav-link[data-id="+window.location.hash.substring(1)+"]").addClass("active");
+		}
+		
+		// Load initially selected
+		$(".nav.nav-link.active").click();
+		
+	},
+	
+	// When any tab gets data error, show message
+	showError :function(message){
+		$(".tabs-content .tab-message").text(message);
+		$(".tabs-content .tab-message").show();
+		$(".tabs-content .tab-html").hide();
+		$(".tabs-content .tab-html").html("");
+	}
+	
+};
--- src/pages/strategy/strategy.js
+++ src/pages/strategy/strategy.js
@@ -1,34 +1,6 @@
 // Initialize KC3 Application
 var app = new KC3();
 
-// Collection of PageTab Wrapper Classes
-var PageTabs = {
-	"home": TabHome,
-	
-	"fleets": TabFleets,
-	// "quests": TabQuests,
-	// "expeds": TabExpeds,
-	
-	"ships": TabShips,
-	"items": TabItems,
-	// "showcase": TabShowcase,
-	
-	"sortie": TabSortie,
-	"e_spring2015": TabE_spring2015,
-	
-	"devmt": TabDevmt,
-	"crafts": TabCrafts,
-	"lscs": TabLscs,
-	"rscs": TabResources,
-	"consumes": TabConsumes
-	
-	// "expcalc": TabExpcalc,
-	// "mods": TabMods,
-};
-
-// Tab currently being loaded
-var tabRequesting = "";
-
 // Page ready
 $(document).on("ready", function(){
 	// General libraries
@@ -41,47 +13,6 @@ $(document).on("ready", function(){
 	app.Resources.init();
 	app.Docks.init();
 	
-	// Test if local player available
-	app.Player.load();
-	if(app.Player.id == 0){
-		$(".navs .nav").hide();
-		return false;
-	}
-	$(".noplayernotice").hide();
-	
-	// Navigation Link onClicks
-	$(".nav.nav-link").on("click", function(){
-		if($(this).hasClass("disabled")){ return false; }
-		
-		if(tabRequesting!=""){ return false; }
-		tabRequesting = $(this).data("id");
-		
-		$(".tabs-content").html("");
-		$(".nav.nav-link").removeClass("active");
-		$(this).addClass("active");
-		window.location = "#"+tabRequesting;
-		
-		$.ajax({
-			url: "html/"+tabRequesting+".html",
-			success: function(response){
-				$(".tabs-content").html(response);
-				PageTabs[ tabRequesting ].init();
-				PageTabs[ tabRequesting ].show();
-				tabRequesting = "";
-			},
-			error: function(){
-				tabRequesting = "";
-			}
-		});
-	});
-	
-	// If there is a hash tag on URL, set it as initial selected
-	if(window.location.hash != ""){
-		$(".nav.nav-link").removeClass("active");
-		$(".nav.nav-link[data-id="+window.location.hash.substring(1)+"]").addClass("active");
-	}
-	
-	// Load initially selected
-	$(".nav.nav-link.active").click();
-	
+	// Execute strategy page
+	app.Strategy.ready();
 });
