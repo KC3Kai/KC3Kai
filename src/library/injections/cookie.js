8b7f42564883c3ddfc8b492340f3a3c5fd083d3b
Jet Rico <jetriconew@gmail.com>
2015-06-17 18:01:03 +0800

    Rewrite Content Scripts

--- src/library/injections/cookie.js
+++ src/library/injections/cookie.js
@@ -1,23 +1,30 @@
-// Wait and listen for cookie activation
-chrome.runtime.onMessage.addListener(function(request, sender, response) {
+/* cookie.js
+KC3改 Cookie Writer
+
+Injected on URLs matching pattern: "*://*.dmm.com/*"
+See Manifest File [manifest.json] under "content_scripts"
+
+Waits and listens for trigger to write cookies.
+Redirects to KanColle game page after writing
+*/
+(function(){
+	"use strict";
 	
-	// Check if we are being called by the menu
-    if(request.game==="kancolle" && request.type==="content"){
-		switch(request.action){
-			
-			// Write the cookies
-			case "activateCookies":
-				document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=osapi.dmm.com;path=/";
-				document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=203.104.209.7;path=/";
-				document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=www.dmm.com;path=/netgame/";
-				document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=log-netgame.dmm.com;path=/";
-				window.location = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/";
-				break;
-				
-			// u w0t m8?
-			default:
-				break;
-		}
+	// Write cookies
+	function writeCookies(){
+		document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=osapi.dmm.com;path=/";
+		document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=203.104.209.7;path=/";
+		document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=www.dmm.com;path=/netgame/";
+		document.cookie = "ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=log-netgame.dmm.com;path=/";
 	}
 	
-});
+	// Wait and listen for cookie activation
+	chrome.runtime.onMessage.addListener(function(request, sender, response) {
+		// Check if we are being called by the menu
+		if((request.identifier || false) == "kc3_cookie"){
+			writeCookies();
+			window.location = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/";
+		}
+	});
+	
+})();
--- src/library/injections/osapi.js
+++ src/library/injections/osapi.js
@@ -1,21 +1,39 @@
-// Initialize global variables
-var intervalChecker;
+/* osapi.js
+KC3改 API Link Extractor
 
-// Looks for API link
-function checkAgain(){
-	// If API link is found
-	if(document.getElementById("externalswf")){
-		// send it to background script
-		chrome.runtime.sendMessage({
-			game:"kancolle",
-			type:"background",
-			action:"set_api_link",
-			swfsrc: document.getElementById("externalswf").getAttribute("src")
-		}, function(response) {
-			// stop interval, and close this window
+Injected on URLs matching pattern: "*://osapi.dmm.com/gadgets/*aid=854854*"
+See Manifest File [manifest.json] under "content_scripts"
+
+Starts a timer every half-second which checks if kancolle game client is present on the DMM webpage
+If detected, passes the API link to background service for saving.
+
+[clearInterval] moved out of the messaging callback to ensure one-time sending only.
+When it was inside the callback, it was executed multiple times if response takes awile.
+Bad side, if it saving on background service failed, no fallback plans but to refresh API link again.
+*/
+(function(){
+	"use strict";
+	
+	// Initialize global variables
+	var intervalChecker;
+
+	// Looks for API link
+	function checkAgain(){
+		// If API link is found
+		if(document.getElementById("externalswf")){
+			// Send it to background script
+			(new RMsg(
+				"service",
+				"set_api_link",
+				{ swfsrc: document.getElementById("externalswf").getAttribute("src") },
+			)).execute();
+			
+			// Stop interval
 			clearInterval(intervalChecker);
-		});
+		}
 	}
-}
-
-intervalChecker = setInterval(checkAgain, 500);
+	
+	// Start timer to check if API link exists every half-second
+	intervalChecker = setInterval(checkAgain, 500);
+	
+})();
