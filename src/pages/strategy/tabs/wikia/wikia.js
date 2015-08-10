07dae81c6e924699fe0db391d9ca4de2d0655b44
= <sin_emerald@hotmail.com>
2015-08-10 03:23:39 -0700

    Add wikiacode exporter to strategy room; update English translations; add Mac files to gitignore

--- src/pages/strategy/tabs/wikia/wikia.js
+++ src/pages/strategy/tabs/wikia/wikia.js
@@ -11,86 +11,97 @@
 		/* INIT
 		Prepares all data needed
 		---------------------------------*/
-		init :function(){
+		init: function(){
 			PlayerManager.loadFleets();
 			console.log(PlayerManager.fleets);
-			
 		},
 		
 		/* EXECUTE
 		Places data onto the interface
 		---------------------------------*/
-		execute :function(){
-			// Empty fleet list
-			$(".tab_fleet .fleet_list").html("");
-			
-			// Execute fleet fill
-			this.showFleet( 0, PlayerManager.fleets[0] );
-			this.showFleet( 1, PlayerManager.fleets[1] );
-			this.showFleet( 2, PlayerManager.fleets[2] );
-			this.showFleet( 3, PlayerManager.fleets[3] );
+		execute: function(){
+      $('.tab_wikia .wikia_list').html('');
+
+      this.show_fleet_code(0, PlayerManager.fleets[0]);
+      this.show_fleet_code(1, PlayerManager.fleets[1]);
+      this.show_fleet_code(2, PlayerManager.fleets[2]);
+      this.show_fleet_code(3, PlayerManager.fleets[3]);
 		},
-		
-		/* Show single fleet
+
+		/* Split the name and remodel with a slash
 		--------------------------------------------*/
-		showFleet :function( index, fleetObj ){
-			if(!fleetObj.active){ return false;}
-			
-			// Create fleet box
-			var fleetBox = $(".tab_fleet .factory .fleet_box").clone().appendTo(".tab_fleet .fleet_list");
-			fleetBox.attr("id", "fleet_box"+index);
-			$(".fleet_name", fleetBox).text( fleetObj.name );
-			
-			// Add ships to fleet box
-			for(var shipCtr in fleetObj.ships){
-				if(fleetObj.ships[shipCtr] > -1){
-					this.showShip( fleetBox , fleetObj.ships[shipCtr]);
-				}
-			}
-			
-			// Show fleet info
-			$(".detail_level .detail_value", fleetBox).text( fleetObj.totalLevel() );
-			$(".detail_los2 .detail_value", fleetBox).text( Math.round( fleetObj.eLos2() * 100) / 100 );
-			$(".detail_los3 .detail_value", fleetBox).text( Math.round( fleetObj.eLos3() * 100) / 100 );
-			$(".detail_air .detail_value", fleetBox).text( fleetObj.fighterPower() );
-			$(".detail_speed .detail_value", fleetBox).text( fleetObj.speed() );
-		},
-		
-		/* Show single ship
+    process_ship_name: function(ship) {
+      var api_name = ship.name();
+      var base_name = api_name
+        .replace(' Kai Ni', '')
+        .replace(' Kai', '')
+        .replace(' zwei', '')
+        .replace(' drei', '')
+        .replace(' Carrier Kai Ni', '')
+        .replace(' Carrier Kai', '')
+        .replace(' Carrier', '');
+
+      // Special case because Chiyoda and Chitose have an "A" remodel which could be part of a name
+      if(api_name.substr(0, 7) == 'Chiyoda' || api_name.substr(0, 7) == 'Chitose') {
+        base_name = base_name.replace(' A', '');
+      }
+
+      var remodel = api_name.split(base_name + ' ')[1] || '';
+
+      return [base_name, remodel];
+    },
+
+		/* Show wikia code for a single fleet
 		--------------------------------------------*/
-		showShip :function( fleetBox, ship_id ){
-			// If ship exists on current list
-			if(KC3ShipManager.get(ship_id) !== false){
-				var thisShip = KC3ShipManager.get(ship_id);
-				var masterShip = thisShip.master();
-				
-				var shipBox = $(".tab_fleet .factory .fleet_ship").clone().appendTo("#"+fleetBox.attr("id")+" .fleet_ships");
-				$(".ship_type", shipBox).text( thisShip.stype() );
-				$(".ship_pic img", shipBox).attr("src", KC3Meta.shipIcon( thisShip.masterId ) );
-				$(".ship_lv_val", shipBox).text( thisShip.level );
-				$(".ship_name", shipBox).text( thisShip.name() );
-				
-				this.showEquip( $(".ship_gear_1", shipBox), thisShip.items[0], thisShip.slots[0] );
-				this.showEquip( $(".ship_gear_2", shipBox), thisShip.items[1], thisShip.slots[1] );
-				this.showEquip( $(".ship_gear_3", shipBox), thisShip.items[2], thisShip.slots[2] );
-				this.showEquip( $(".ship_gear_4", shipBox), thisShip.items[3], thisShip.slots[3] );
-			}
-		},
-		
-		/* Show single equipment
+    show_fleet_code: function(index, fleet) {
+      if(!fleet.active) return false;
+
+      // Create the box and apply basic values
+      var box = $('.tab_wikia .factory .wikia_box').clone().appendTo('.tab_wikia .wikia_list');
+      box.attr('id', 'wikia_box' + index);
+      $('.wikia_name', box).text(fleet.name);
+
+      // Start up the code string
+      var code = "{{EventComp\n";
+
+      // Iterate over the ships in this fleet
+      for(var i = 0; i < fleet.ships.length; i++) {
+        if(fleet.ships[i] > -1) code += this.show_ship_code(box, fleet.ships[i]);
+      }
+
+      code += "| hq = " + PlayerManager.hq.level + "\n";
+      code += "}}";
+
+      $('.wikia_code', box).text(code);
+    },
+
+		/* Show wikia code for a single ship
 		--------------------------------------------*/
-		showEquip :function( gearBox, gear_id, capacity){
-			if(gear_id > -1){
-				var thisItem = KC3GearManager.get(gear_id);
-				var masterItem = thisItem.master();
-				
-				$("img", gearBox).attr("src", "../../assets/img/items/"+masterItem.api_type[3]+".png");
-				$(".gear_name", gearBox).text( thisItem.name() );
-			}else{
-				gearBox.hide();
-			}
-		}
-		
+    show_ship_code: function(box, ship_id) {
+      var ship = KC3ShipManager.get(ship_id);
+      var ship_name = this.process_ship_name(ship).join('/');
+      var code = '';
+
+      code += "| " + ship_name  + "\n";
+      code += "| " + ship.level + "\n";
+
+      code += this.show_equip_code(ship.items[0]);
+      code += this.show_equip_code(ship.items[1]);
+      code += this.show_equip_code(ship.items[2]);
+      code += this.show_equip_code(ship.items[3]);
+
+      code += "|-\n";
+
+      return code;
+    },
+
+    show_equip_code: function(equip_id) {
+      if(equip_id > -1) {
+        var equip = KC3GearManager.get(equip_id);
+        return "| " + equip.name() + "\n";
+      } else {
+        return '';
+      }
+    },
 	};
-	
 })();
