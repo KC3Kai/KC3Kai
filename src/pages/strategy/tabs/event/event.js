c29b4bfe28e4fac2a6961caac659377eab3b315c
Rei Hakurei <reimu_after_marisa@yahoo.com>
2015-08-28 20:51:37 +0700

    Conceal 9999 HP incase the difficulty is already chosen
    _(:3 help

--- src/pages/strategy/tabs/event/event.js
+++ src/pages/strategy/tabs/event/event.js
@@ -80,7 +80,10 @@
 							// If HP-based gauge
 							if(typeof element.maxhp != "undefined"){
 								if(element.curhp>1){ // i want to approach last kill as JUST DO IT instead leaving 1HP only.
-									$(".map_hp_txt", mapBox).text( element.curhp+" / "+element.maxhp );
+									if((element.maxhp === 9999) || (element.curhp === 9999))
+										$(".map_hp_txt", mapBox).text( "???? / ????" );
+									else
+										$(".map_hp_txt", mapBox).text( element.curhp+" / "+element.maxhp );
 									$(".map_bar", mapBox).css("width", ((element.curhp/element.maxhp)*80)+"px");
 								}else{
 									mapBox.addClass("noclearnogauge");
