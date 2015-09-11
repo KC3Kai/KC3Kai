beabaddb2a1044902292dfeb5ff031ce5dee6862
Nekoworkshop <nekoworkshop@gmail.com>
2015-09-11 16:13:07 -0400

    Implemented Deckbuilder feature in main panel.

--- src/pages/devtools/themes/natsuiro/natsuiro.js
+++ src/pages/devtools/themes/natsuiro/natsuiro.js
@@ -101,6 +101,66 @@
 			})).execute();
 		});
 		
+		// Export button
+		$(".module.controls .btn_export").on("click", function(){
+			window.open("http://www.kancolle-calc.net/deckbuilder.html?predeck=".concat(encodeURI(
+					JSON.stringify({
+						"version":3,
+						"f1":generate_fleet_JSON(PlayerManager.fleets[0]),
+						"f2":generate_fleet_JSON(PlayerManager.fleets[1]),
+						"f3":generate_fleet_JSON(PlayerManager.fleets[2]),
+						"f4":generate_fleet_JSON(PlayerManager.fleets[3]),
+						})
+				)));
+				
+			console.log("http://www.kancolle-calc.net/deckbuilder.html?predeck=".concat(encodeURI(
+					JSON.stringify({
+						"version":3,
+						"f1":generate_fleet_JSON(PlayerManager.fleets[0]),
+						"f2":generate_fleet_JSON(PlayerManager.fleets[1]),
+						"f3":generate_fleet_JSON(PlayerManager.fleets[2]),
+						"f4":generate_fleet_JSON(PlayerManager.fleets[3]),
+						})
+				)));
+		});
+		
+    
+		/* Code for generating deckbuilder style JSON data.
+		--------------------------------------------*/
+		function generate_fleet_JSON(fleet) {
+    	var result = {};
+    	for(var i = 0; i < fleet.ships.length; i++) {
+    		if(fleet.ships[i] > -1){
+    			result["s".concat(i+1)] = generate_ship_JSON(fleet.ships[i]);
+    		}
+    	}
+    	return result;
+    };
+    
+    function generate_ship_JSON (ship_ID) {
+    	var result = {};
+    	var ship = KC3ShipManager.get(ship_ID);
+    	result.id = ship.masterId;
+    	result.lv = ship.level;
+    	result.luck = ship.lk[0];
+    	result.items = generate_equipment_JSON(ship);
+    	return result;
+    };
+    
+    function generate_equipment_JSON (shipObj) {
+    	var result = {};
+    	for(var i = 0; i < 4; i++) {
+    		if(shipObj.items[i]> -1){
+    			result["i".concat(i+1)] ={
+    					"id":KC3GearManager.get(shipObj.items[i]).masterId,
+    					"rf":KC3GearManager.get(shipObj.items[i]).stars
+    			};
+    		} else {break;}
+    	}
+    	return result;
+    };
+		
+		
 		// Switching Activity Tabs
 		$(".module.activity .activity_tab").on("click", function(){
 			// if($(this).data("target")===""){ return false; }
