860616124e769a97f4c2320fb567b88166a9da33
McDohl <McDohl@users.noreply.github.com>
2015-10-10 10:20:26 +0700

    change LoS image
    change to New, Old and Sigma instead of Eq, To and no name

--- src/pages/devtools/themes/natsuiro/natsuiro.js
+++ src/pages/devtools/themes/natsuiro/natsuiro.js
@@ -78,7 +78,7 @@
 		// eLoS Toggle
 		$(".summary-eqlos").on("click",function(){
 			ConfigManager.scrollElosMode();
-			$(".summary-eqlos .summary_icon img").attr("src", "../../../../assets/img/stats/"+["lsc","lst","lse","ls"][ConfigManager.elosFormula]+".png");
+			$(".summary-eqlos img", self.domElement).attr("src", "../../../../assets/img/stats/los"+ConfigManager.elosFormula+".png"); 
 			$(".summary-eqlos .summary_text").text( Math.round(((selectedFleet < 5) ? PlayerManager.fleets[selectedFleet-1].eLoS() : PlayerManager.fleets[0].eLoS()+PlayerManager.fleets[1].eLoS()) * 100) / 100 );
 		}).addClass("hover");
 		
