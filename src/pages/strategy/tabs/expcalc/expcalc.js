45d8ae1ad837f90831b488dcf626c5076a07f402
YukiNyaa712 <yukinyaa712@gmail.com>
2016-01-18 21:45:56 +0900

    implementation of #1102

--- src/pages/strategy/tabs/expcalc/expcalc.js
+++ src/pages/strategy/tabs/expcalc/expcalc.js
@@ -227,10 +227,12 @@
 			
 			// Exp Modifier: MVP
 			$(".ship_mvp .ship_value", goalBox).text( grindData[6]?"Yes":"No" );
+			$(".ship_mvp .ship_value", goalBox).style.color = (grindData[6]? "#e33" : "#039");
 			if(grindData[6]===1){ expPerSortie = expPerSortie * 2; }
 			
 			// Exp Modifier: FLAGSHIP
 			$(".ship_fs .ship_value", goalBox).text( grindData[5]?"Yes":"No" );
+			$(".ship_fs .ship_value", goalBox).style.color = (grindData[5]? "#e33" : "#039");
 			if(grindData[5]===1){ expPerSortie = expPerSortie * 1.5; }
 			
 			// Exp Modifier: RANK
