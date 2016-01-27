fc03492d979344c9cfc5c77e151d5c41479fd178
YukiNyaa712 <yukinyaa712@gmail.com>
2016-01-28 01:03:44 +0900

    Help me stackoverflow!
    this shit fucks everything up

--- src/pages/strategy/tabs/savedata/savedata.js
+++ src/pages/strategy/tabs/savedata/savedata.js
@@ -5,7 +5,6 @@
 	
 	KC3StrategyTabs.savedata.definition = {
 		tabSelf: KC3StrategyTabs.savedata,
-		text:"",
 		/* INIT
 		Prepares all data needed
 		---------------------------------*/
@@ -13,44 +12,37 @@
 
 		},
 		tmptext:"",
-		db2arr : function(){
-		    var text="";
-		    $(".tab_savedata .tmptextbox").empty();
-		    window.KC3Database.con.tables.forEach( //access all tables
-            function(table){
-                  var wait=true;
-                  table.toArray(function(all) { //add table data tmptext
-                   $(".tab_savedata .tmptextbox").append(
-                          "\""+table.name+"\":"+
-                          JSON.stringify(all)+
-                          ","
-                        ).then(function(){
-                          wait=false;
-                        });
-                  });
-                  while(wait){}
-            }
-        );
-        while ( text === "" ) {
-          text = $(".tab_savedata .tmptextbox").text().substring(0,-1);
-        }
-        $(".tab_savedata .tmptextbox").empty();
-        return text;
-    },
+	
 		/* EXECUTE
 		Places data onto the interface
 		---------------------------------*/
 		execute :function(){
         //save stuffs
-       
+       	var db2arr = function(){
+  		    var text=new StringBuilder();
+  		    $(".tab_savedata .tmptextbox").empty();
+  		    window.KC3Database.con.tables.forEach( //access all tables
+              function(table){
+                    table.toArray(function(all) { //add table data tmptext
+                     //$(".tab_savedata .tmptextbox").append(
+                          text.Append(
+                            "\""+table.name+"\":"+
+                            JSON.stringify(all)+
+                            ","
+                          );
+                    });
+              }
+          );
+          //text = $(".tab_savedata .tmptextbox").text().substring(0,-1);
+          //$(".tab_savedata .tmptextbox").empty();
+          return text.ToString().substring(0,-1);
+       	}
         
         $(".tab_savedata .export_data").on("click", function(){
-  				var blob = new Blob([this.db2arr()], {type: "application/json;charset=utf-8"});
-  				saveAs(blob, "["+PlayerManager.hq.name+"] "+((new Date()).format("yyyy-mm-dd"))+".kc3db");
+  				alert( db2arr() );
+  				var blob = new Blob([db2arr()], {type: "application/json;charset=utf-8"});
+  				//saveAs(blob, "["+PlayerManager.hq.name+"] "+((new Date()).format("yyyy-mm-dd"))+".kc3db");
   			});
-  			var text;
-  			this.db2arr();
-        alert(this.text);
        
           
 		}
