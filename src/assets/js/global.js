d4856cf63310239ea7b196c114de69fe6dc1747a
dragonjet <jetriconew@gmail.com>
2016-02-28 00:53:14 +0900

    Close #1163 Export recorded histories to CSV Also moves forward #1106

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -219,6 +219,19 @@ String.prototype.plusCurrentTime = function() {
 	return String(timeFinished).toHHMMSS();
 };
 
+/* hashing for integrity checks
+-----------------------------------*/
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
 /*******************************\
 |*** Number                     |
 \*******************************/
@@ -608,4 +621,3 @@ Storage.prototype.getObject = function(key) {
 		},
 	});
 }).call(Element);
-
