108797c992f91f613173715d99b0be18fc4b692d
Javran Cheng <Javran.c@gmail.com>
2016-05-10 02:49:25 -0400

    build up UI based on KC3Fleet

--- src/pages/strategy/tabs/fleet/fleet.js
+++ src/pages/strategy/tabs/fleet/fleet.js
@@ -52,31 +52,77 @@
 			
 			var fleets = this.loadCurrentFleets();
 
-
 			$.each(fleets, function(ind, fleet) {
 				var kcFleet = self.createKCFleetObject( fleet );
-				console.log( kcFleet );
-				console.log( kcFleet.totalLevel() );
-				// TODO
-				console.log( Math.round( kcFleet.eLos2() * 100) / 100 );
-				// TODO
-				console.log( Math.round( kcFleet.eLos3() * 100) / 100 );
-				console.log( kcFleet.fighterPowerText() );
-				console.log( kcFleet.speed() );
+				self.showKCFleet( kcFleet );
 			});
+		},
 
-			// Execute fleet fill
-			this.showFleet( 0, PlayerManager.fleets[0] );
-			this.showFleet( 1, PlayerManager.fleets[1] );
-			this.showFleet( 2, PlayerManager.fleets[2] );
-			this.showFleet( 3, PlayerManager.fleets[3] );
+		/* Show single fleet
+		   --------------------------------------------*/
+		showKCFleet: function(kcFleet) {
+			if (!kcFleet.active) return;
+			var self = this;
+
+			// Create fleet box
+			var fleetBox = $(".tab_fleet .factory .fleet_box").clone().appendTo(".tab_fleet .fleet_list");
+			// fleetBox.attr("id", "fleet_box"+index);
+			$(".fleet_name", fleetBox).text( kcFleet.name );
+
+			$.each( [0,1,2,3,4,5], function(_,ind) {
+				var kcShip = kcFleet.ship(ind);
+				self.showKCShip(fleetBox, kcShip);
+			});
+			
+			// Show fleet info
+			$(".detail_level .detail_value", fleetBox).text( kcFleet.totalLevel() );
+			$(".detail_los2 .detail_value", fleetBox).text( Math.round( kcFleet.eLos2() * 100) / 100 );
+			$(".detail_los3 .detail_value", fleetBox).text( Math.round( kcFleet.eLos3() * 100) / 100 );
+			$(".detail_air .detail_value", fleetBox).text( kcFleet.fighterPowerText() );
+			$(".detail_speed .detail_value", fleetBox).text( kcFleet.speed() );
+		},
+
+		/* Show single ship
+		   --------------------------------------------*/
+		showKCShip: function(fleetBox, kcShip) {
+			if (kcShip.masterId === 0) return;
+			
+			var self = this;
+			var shipBox = $(".tab_fleet .factory .fleet_ship").clone();
+			$(".fleet_ships", fleetBox).append( shipBox );
+
+			$(".ship_type", shipBox).text( kcShip.stype() );
+			$(".ship_pic img", shipBox).attr("src", KC3Meta.shipIcon( kcShip.masterId ) );
+			$(".ship_lv_val", shipBox).text( kcShip.level );
+			$(".ship_name", shipBox).text( kcShip.name() );
+
+			$.each([0,1,2,3], function(_,ind) {
+				self.showKCGear(
+					$(".ship_gear_"+(ind+1), shipBox),
+					kcShip.equipment(ind),
+					kcShip.slots[ind]);
+			});
+		},
+		
+		/* Show single equipment
+		   --------------------------------------------*/
+		showKCGear: function(gearBox, kcGear, capacity) {
+			if (kcGear.masterId === 0) {
+				gearBox.hide();
+				return;
+			}
+
+			var masterData = kcGear.master();
+			$("img", gearBox).attr("src", "../../assets/img/items/"+masterData.api_type[3]+".png");
+			$(".gear_name", gearBox).text( kcGear.name() );
 		},
 
 		createKCFleetObject: function(fleetObj) {
 			var fleet = new KC3Fleet();
 			fleet.name = fleetObj.name;
 			fleet.ships = [1,2,3,4,5,6];
-
+			if (!fleetObj) return;
+			fleet.active = true;
 			var shipObjArr = [];
 
 			// simulate ShipManager
@@ -98,8 +144,8 @@
 				ship.rosterId = fleet.ships[ind];
 				ship.masterId = shipObj.id;
 				ship.level = shipObj.level;
-				// TODO: need to calculate LoS
-
+				// calculate naked LoS
+				ship.ls[0] = ship.estimateNakedLoS() + ship.equipmentTotalLoS();
 				ship.lk[0] = shipObj.luck;
 				ship.items = [1,2,3,4];
 				ship.slots = masterData.api_maxeq;
@@ -162,7 +208,6 @@
 						luck: ship.lk[0],
 						equipments: convertEquipmentsOf(ship)
 					};
-
 					fleetObjShips.push( shipObj );
 				});
 				var fleetObj = {
@@ -178,68 +223,7 @@
 			});
 
 			return fleetsObj;
-		},
-		
-		/* Show single fleet
-		   --------------------------------------------*/
-		showFleet :function( index, fleetObj ){
-			if(!fleetObj.active){ return false;}
-			
-			// Create fleet box
-			var fleetBox = $(".tab_fleet .factory .fleet_box").clone().appendTo(".tab_fleet .fleet_list");
-			fleetBox.attr("id", "fleet_box"+index);
-			$(".fleet_name", fleetBox).text( fleetObj.name );
-			
-			// Add ships to fleet box
-			for(var shipCtr in fleetObj.ships){
-				if(fleetObj.ships[shipCtr] > -1){
-					this.showShip( fleetBox , fleetObj.ships[shipCtr]);
-				}
-			}
-			
-			// Show fleet info
-			$(".detail_level .detail_value", fleetBox).text( fleetObj.totalLevel() );
-			$(".detail_los2 .detail_value", fleetBox).text( Math.round( fleetObj.eLos2() * 100) / 100 );
-			$(".detail_los3 .detail_value", fleetBox).text( Math.round( fleetObj.eLos3() * 100) / 100 );
-			$(".detail_air .detail_value", fleetBox).text( fleetObj.fighterPowerText() );
-			$(".detail_speed .detail_value", fleetBox).text( fleetObj.speed() );
-		},
-		
-		/* Show single ship
-		   --------------------------------------------*/
-		showShip :function( fleetBox, ship_id ){
-			// If ship exists on current list
-			if(KC3ShipManager.get(ship_id) !== false){
-				var thisShip = KC3ShipManager.get(ship_id);
-				var masterShip = thisShip.master();
-				
-				var shipBox = $(".tab_fleet .factory .fleet_ship").clone().appendTo("#"+fleetBox.attr("id")+" .fleet_ships");
-				$(".ship_type", shipBox).text( thisShip.stype() );
-				$(".ship_pic img", shipBox).attr("src", KC3Meta.shipIcon( thisShip.masterId ) );
-				$(".ship_lv_val", shipBox).text( thisShip.level );
-				$(".ship_name", shipBox).text( thisShip.name() );
-				
-				this.showEquip( $(".ship_gear_1", shipBox), thisShip.items[0], thisShip.slots[0] );
-				this.showEquip( $(".ship_gear_2", shipBox), thisShip.items[1], thisShip.slots[1] );
-				this.showEquip( $(".ship_gear_3", shipBox), thisShip.items[2], thisShip.slots[2] );
-				this.showEquip( $(".ship_gear_4", shipBox), thisShip.items[3], thisShip.slots[3] );
-			}
-		},
-		
-		/* Show single equipment
-		   --------------------------------------------*/
-		showEquip :function( gearBox, gear_id, capacity){
-			if(gear_id > -1){
-				var thisItem = KC3GearManager.get(gear_id);
-				var masterItem = thisItem.master();
-				
-				$("img", gearBox).attr("src", "../../assets/img/items/"+masterItem.api_type[3]+".png");
-				$(".gear_name", gearBox).text( thisItem.name() );
-			}else{
-				gearBox.hide();
-			}
 		}
-		
 	};
 	
 })();
