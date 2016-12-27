48ed54a0fe7cfb66593872090ab18b3e9dd5e3ae
sin_sin <sinsinpub@gmail.com>
2016-12-28 00:57:21 +0800

    Refactor ship name meta method, support direct affixes translation

--- src/library/modules/Translation.js
+++ src/library/modules/Translation.js
@@ -102,13 +102,13 @@
 			// Japanese special case where ships and items sources are already in JP
 			if(
 				(["jp", "tcn"].indexOf(language) > -1)
-				&& (filename==="ships" || filename==="items")
+				&& (["ships", "items", "ship_affix"].indexOf(filename) > -1)
 			){
 				extendEnglish = false;
 			}
 			// make ships.json and items.json an option to be always in specified one
 			if (!!info_force_ship_lang
-				&& (filename==="ships" || filename==="items")){
+				&& (["ships", "items", "ship_affix"].indexOf(filename) > -1)){
 				extendEnglish = false;
 				language = info_force_ship_lang;
 			}
