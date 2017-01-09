da8c6385a4af22df84111c3342aa981563915169
Javran Cheng <Javran.c@gmail.com>
2017-01-09 00:17:56 -0500

    creating expedtable sr tab

---
+++ src/pages/strategy/tabs/expedtable/expedtable.js
@@ -0,0 +1,43 @@
+(function(){
+	"use strict";
+
+	KC3StrategyTabs.expedtable = new KC3StrategyTab("expedtable");
+
+	KC3StrategyTabs.expedtable.definition = {
+		tabSelf: KC3StrategyTabs.expedtable,
+
+		/* INIT: mandatory
+		Prepares initial static data needed.
+		---------------------------------*/
+		init: function() {
+		},
+
+		/* RELOAD: optional
+		Loads latest player or game data if needed.
+		---------------------------------*/
+		reload: function() {
+		},
+
+		/* EXECUTE: mandatory
+		Places data onto the interface from scratch.
+		---------------------------------*/
+		execute: function() {
+		},
+
+		/* UPDATE: optional
+		Partially update elements of the interface,
+			possibly without clearing all contents first.
+		Be careful! Do not only update new data,
+			but also handle the old states (do cleanup).
+		Return `false` if updating all needed,
+			EXECUTE will be invoked instead.
+		---------------------------------*/
+		update: function(pageParams) {
+			// Use `pageParams` for latest page hash values,
+			// KC3StrategyTabs.pageParams keeps the old values for states tracking
+
+			// Returning `true` means updating has been handled.
+			return false;
+		}
+	};
+})();
