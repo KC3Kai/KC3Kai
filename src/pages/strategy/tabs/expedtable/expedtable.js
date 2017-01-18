3a1bfef70e83ad8215091ec891d5add4f73f95d4
Javran Cheng <Javran.c@gmail.com>
2017-01-17 19:37:26 -0500

    put some contents on exped tab

--- src/pages/strategy/tabs/expedtable/expedtable.js
+++ src/pages/strategy/tabs/expedtable/expedtable.js
@@ -22,6 +22,53 @@
 		Places data onto the interface from scratch.
 		---------------------------------*/
 		execute: function() {
+			var ExpedInfo = PS["KanColle.Expedition.New.Info"];
+			var factory = $(".tab_expedtable .factory");
+			var expedTableRoot = $("#exped_table_content_root");
+
+			var allExpeds = [];
+			var i;
+			for (i=1; i<=40; ++i)
+				allExpeds.push(i);
+
+			function makeWinItem( jqObj, winItemArr ) {
+				var itemId = winItemArr[0];
+				var idToItem = {
+					1: "bucket",
+					2: "ibuild",
+					3: "devmat",
+					10: "box1",
+					11: "box2",
+					12: "box3"
+				};
+				if (itemId !== 0) {
+					jqObj
+						.append($(
+							"<img>",
+							{src: "../../assets/img/client/"+idToItem[itemId]+".png"}))
+					    .append("x" + winItemArr[1]);
+				} else {
+					jqObj.text( "-" );
+				}
+			}
+
+			allExpeds.forEach( function(eId) {
+				var expedRow = $(".exped_row", factory).clone();
+				var resourceInfo = ExpedInfo.getInformation( eId ).resource;
+				var masterInfo = KC3Master._raw.mission[eId];
+
+				$(".info_col.id", expedRow).text( eId );
+				$(".info_col.time", expedRow).text( String( 60 * masterInfo.api_time ).toHHMMSS() );
+
+				["fuel", "ammo", "steel", "bauxite"].forEach( function(name) {
+					$(".info_col." + name, expedRow).text( resourceInfo[name] );
+				});
+
+				makeWinItem( $(".info_col.item1", expedRow), masterInfo.api_win_item1 );
+				makeWinItem( $(".info_col.item2", expedRow), masterInfo.api_win_item2 );
+
+				expedTableRoot.append( expedRow );
+			});
 		},
 
 		/* UPDATE: optional
