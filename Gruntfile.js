KC3Kai/KC3Kai

commit 01c1162cfc4fad9f4a2a2e7d7de75f6be2b0cce0

AutoRestart: Remove unused terms +.format() + TL pointer

Gruntfile.js
@@ -169,8 +169,8 @@ module.exports = function(grunt) {
 				options: {
 					replacements: [
 						{
-							pattern: /assets\/js\/jquery\-2\.1\.3\.min\.js/ig,
-							replacement: 'assets/js/global.js'
+							pattern: /"assets\/js\/jquery\-2\.1\.3\.min\.js",/ig,
+							replacement: ''
 						},
 						{
 							pattern: /library\/objects\/Messengers\.js/ig,

src/library/modules/Service.js
@@ -355,7 +355,7 @@ See Manifest File [manifest.json] under "background" > "scripts"
 				chrome.notifications.create("kc3kai_update", {
 					type: "basic",
 					iconUrl: chrome.extension.getURL("assets/img/logo/128.png"),
-					title: KC3Meta.term("UpdateNotifTitle").replace("{0}", details.version),
+					title: KC3Meta.term("UpdateNotifTitle").format(details.version),
 					message: KC3Meta.term("UpdateNotifText"),
 					buttons: [
 						{ title: KC3Meta.term("PageUpdateRestartNow") },

commit f277004ffd445922ebdf207c603611f08f906874

Fix grunt built manifest lost all jquery issue caused by 01c1162
for built ver, jquery.min.js is combined with global.js,
so all jquery.min.js should be replaced with global.js

Gruntfile.js
@@ -187,8 +187,12 @@ module.exports = function(grunt) {
 				options: {
 					replacements: [
 						{
-							pattern: /"assets\/js\/jquery\-2\.1\.3\.min\.js",/ig,
+							pattern: /\"assets\/js\/\.\.\/js\/global\.js\",/ig,
 							replacement: ''
+						},
+						{
+							pattern: /assets\/js\/jquery\-2\.1\.3\.min\.js,/ig,
+							replacement: 'assets/js/global.js'
 						},
 						{
 							pattern: /library\/objects\/Messengers\.js/ig,