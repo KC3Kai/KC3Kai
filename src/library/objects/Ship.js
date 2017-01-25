6add5c37a206ce00e4891cf3406a2686aa089674
Javran Cheng <Javran.c@gmail.com>
2017-01-25 11:03:34 -0500

    Arashio K2 can equip daihatsu.

--- src/library/objects/Ship.js
+++ src/library/objects/Ship.js
@@ -602,13 +602,13 @@ KC3改 Ship Object
 		
 		// only few DDs and CLs are capable of equipping daihatsu
 		// including:
-		// Abukuma K2(200), Verniy(147), Ooshio K2(199),
+		// Abukuma K2(200), Verniy(147), Ooshio K2(199), Arashio K2(490)
 		// Satsuki K2(418), Mutsuki K2(434), Kisaragi K2(435),
 		// Kasumi K2(464), Kasumi K2B(470),
 		// Asashio K2D(468), Kawakaze K2(469),
 		// Kinu K2(487)
 		if ([2,3].indexOf( master.api_stype ) !== -1 &&
-			[147,199,200,418,434,435,464,470,468,469,487].indexOf( this.masterId ) === -1)
+			[147,199,200,418,434,435,464,470,468,469,487,490].indexOf( this.masterId ) === -1)
 			return false;
 		return true;
 	};
