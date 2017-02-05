a1a774caff7e60a82acc04067ab0a9aa072925c3
Javran Cheng <Javran.c@gmail.com>
2017-02-05 14:51:58 -0500

    rework on expedscorer UI

--- src/pages/strategy/tabs/expedscorer/expedscorer.js
+++ src/pages/strategy/tabs/expedscorer/expedscorer.js
@@ -1,6 +1,22 @@
 (function(){
 	"use strict";
 
+	let ExpedInfo = PS["KanColle.Expedition.New.Info"];
+
+	let enumFromTo = Expedition.enumFromTo;
+	let saturate = Expedition.saturate;
+
+	// - a preset overwrites user choices
+	// - recommended: exclude some non-beneficial expeditions
+	// - buckets: all expeditions that might yield buckets. (item2 included, without considering GS)
+	let recommendedBlacklist = [22,29,30,31,33,34];
+	let presets = {
+		"all": Expedition.allExpedIds,
+		"recommended": Expedition.allExpedIds.filter( x => recommendedBlacklist.indexOf(x) === -1),
+		"buckets": [2,4,9,10,11,13,14,18,24,26,36,39,40],
+		"none": []
+	};
+
 	KC3StrategyTabs.expedscorer = new KC3StrategyTab("expedscorer");
 
 	KC3StrategyTabs.expedscorer.definition = {
@@ -10,146 +26,183 @@
 		Prepares all data needed
 		---------------------------------*/
 		init :function(){
+		},
 
+		setupControls() {
+			let factoryRoot = $(".tab_expedscorer .factory");
+			let jqRoot = $(".tab_expedscorer #exped_scorer_control_root");
+			let jqControlRow1 = $(".control_row.row1", jqRoot);
+
+			// setup expedition checkboxes
+			enumFromTo(1,5).map( world => {
+				let jqBox = $(".control_box", factoryRoot).clone();
+				jqBox.data("world", world);
+				jqBox.addClass("world");
+
+				$(".header",jqBox).text("World " + world);
+
+				enumFromTo(8*world-7, 8*world).map( eId => {
+					let jqExpedEntry = $("label.exped_toggle", factoryRoot).clone();
+					jqExpedEntry.data("eid", eId);
+					$(".name",jqExpedEntry).text( eId );
+					let timeText = (ExpedInfo.getInformation(eId).timeInMin*60)
+						.toString()
+						.toHHMMSS()
+						.substring(0,5);
+					$(".time",jqExpedEntry).text( timeText );
+					jqBox.append( jqExpedEntry );
+				});
+				jqBox.insertBefore($(">.control_box.preset", jqControlRow1));
+			});
+
+			let jqExpeds = $(".control_box.world .exped_toggle", jqRoot);
+			// setup preset buttons
+			$(".control_box.preset button",jqRoot).click( function() {
+				let jq = $(this);
+				let preset = presets[ jq.data("name") ];
+				console.assert( typeof preset !== "undefined" );
+				jqExpeds.each( function() {
+					let jq = $(this);
+					$("input[type=checkbox]", jq)
+						.prop("checked", preset.indexOf(jq.data("eid")) !== -1);
+				});
+			});
+
+			let jqControlRow2 = $(".control_row.row2", jqRoot);
+			let jqResourceControl = $(".control_box.resource .content", jqControlRow2);
+
+			// setup resource priority sliders
+			// note that resources are handled in f/s/a/b order
+			// to match the in-game resource panel layout
+			["fuel","steel","ammo","bauxite"].map( (resourceName, ind) => {
+				let jqBox = $(".resource_box", factoryRoot).clone();
+				jqBox.addClass(resourceName);
+				$("img",jqBox).attr("src", "../../assets/img/client/"+resourceName+".png");
+				let jqView = $(".view", jqBox);
+				let sliderSettings = {
+					min: -5, max: 20, step: 1,
+					tooltip: "hide"
+				};
+				let slider = $("input.slider",jqBox)
+					.slider(sliderSettings)
+					.change(function (e) {
+						jqView.text(e.value.newValue);
+					})
+					.trigger( $.Event("change", {value: {newValue: 5}}) );
+				jqBox.data("slider",slider);
+				jqBox.data("name",resourceName);
+				jqResourceControl.append(jqBox);
+				if (ind === 1 || ind === 3) {
+					jqResourceControl.append( $("<div></div>").addClass("clear") );
+				}
+			});
+			$(".control_box.calc button.calc").click( () => this.computeResults() );
+
+			// setup default values (TODO: will save changes to localStorage)
+			$(".control_box.preset button[data-name=recommended]",jqRoot).click();
+			$(".control_box.fleet input[type=radio][name=fleet_count][value=3]",jqRoot)
+				.prop("checked",true);
+		},
+
+		computeResults() {
+			let jqRoot = $(".tab_expedscorer #exped_scorer_control_root");
+			let calcBtn = $(".control_box.calc button.calc");
+			// prevent running same computation more than once if user has clicked too quick
+			calcBtn.prop("disabled", true);
+
+			let resultTable = $(".tab_expedscorer .results tbody");
+			resultTable.empty();
+
+			let candidateIds = [];
+			$(".control_box.world .exped_toggle",jqRoot)
+				.each(function() {
+					if ( $("input[type=checkbox]", this).prop("checked") ) {
+						candidateIds.push( $(this).data("eid") );
+					}
+				});
+
+			let resourceWeight = {};
+			$(".resource_box", jqRoot).each( function() {
+				let jq = $(this);
+				let name = jq.data("name");
+				let weight = jq.data("slider").slider("getValue");
+				resourceWeight[name]=weight;
+			});
+
+			// get afk time, and write back normalized values
+			let jqHrs = $(".control_box.afktime input[type=text][name=hrs]",jqRoot);
+			let jqMins = $(".control_box.afktime input[type=text][name=mins]",jqRoot);
+			let afkHH = parseInt( jqHrs.val() , 10);
+			let afkMM = parseInt( jqMins.val(), 10);
+			jqHrs.val(afkHH);
+			jqMins.val(afkMM);
+
+			let afkTimeInMin = afkHH*60 + afkMM;
+
+			let fleetCount = parseInt( $(".control_box.fleet input[type=radio]:checked", jqRoot).val(), 10);
+			console.assert( typeof fleetCount === "number" &&
+							fleetCount > 0 && fleetCount <= 3);
+
+
+			// make sure we reload config every time
+			// so user can also have expedtable opened and whatever changes made there
+			// gets used immediately here.
+			let config = Expedition.loadExpedConfig(true);
+
+			// only compute income table for selected elements
+			let infoTable = {};
+			candidateIds.map( (eId) => {
+				let info = ExpedInfo.getInformation(eId);
+				infoTable[eId] = Expedition.computeNetIncome(config[eId],eId);
+				infoTable[eId].timeInMin = Math.max(afkTimeInMin, info.timeInMin);
+			});
+
+			// mark score on income
+			function markScore(income) {
+				income.score = ["fuel","ammo","steel","bauxite"]
+					.map( resourceName => income[resourceName] * resourceWeight[resourceName] )
+					.reduce( (a,b) => (a+b), 0);
+				return;
+			}
+
+			let results = Expedition
+				.chooseN(candidateIds, fleetCount)
+				.map( function( expedSet ) {
+					let retVal = {
+						set: expedSet,
+						fuel: 0, ammo: 0, steel: 0, bauxite: 0
+					};
+					expedSet.map( function (eId) {
+						let info = infoTable[eId];
+						["fuel","ammo","steel","bauxite"].map( function(resourceName) {
+							retVal[resourceName] += info[resourceName] * 60 / info.timeInMin;
+						});
+					});
+					markScore(retVal);
+					return retVal;
+				})
+				.sort( (a,b) => b.score - a.score )
+				.slice(0,50); // take only top 50
+
+			results.map( function (curVal) {
+				let row = $('<tr></tr>');
+				$('<td></td>').text(curVal.set.join(",")).appendTo(row);
+				$('<td></td>').text(curVal.fuel.toFixed(2)).appendTo(row);
+				$('<td></td>').text(curVal.ammo.toFixed(2)).appendTo(row);
+				$('<td></td>').text(curVal.steel.toFixed(2)).appendTo(row);
+				$('<td></td>').text(curVal.bauxite.toFixed(2)).appendTo(row);
+				$('<td></td>').text(curVal.score.toFixed(2)).appendTo(row);
+				resultTable.append( row );
+			});
+			calcBtn.prop("disabled", false);
 		},
 
 		/* EXECUTE
 		Places data onto the interface
 		---------------------------------*/
 		execute :function(){
-
-			// Add all expedition numbers on the list
-			var KE = PS["KanColle.Expedition"];
-			$('.tab_expedscorer .expedNumbers').html("");
-			KE.allExpeditions.forEach( function(curVal, ind) {
-				var row = $('.tab_expedscorer .factory .expedNum').clone();
-				$(".expedCheck input", row).attr("value", curVal.id.toString());
-				$(".expedText", row).text( curVal.id.toString() );
-				$(".expedTime", row).text( (curVal.cost.time*60).toString().toHHMMSS().substring(0,5) );
-
-				var boxNum = Math.ceil((ind+1)/8);
-				$(".tab_expedscorer .expedNumBox_"+boxNum).append( row );
-			});
-
-			// Add world toggle
-			$(".tab_expedscorer .expedNumBox")
-				.filter(function(i,x){return $(x).hasClass("expedNumBox_"+(i+1));})
-				.each(function(i,x){
-					var
-						row = $('.tab_expedscorer .factory .expedNum').clone().addClass("expedWhole").removeClass("expedNum"),
-						val = true;
-					$("input",".expedNumBox_"+(i+1)).each(function(id,elm){
-						val&= $(elm).prop("checked");
-					});
-					$(row)
-						.find(".expedCheck input")
-							.attr("value", i+1)
-							.prop("checked", val)
-						.end()
-						.find(".expedText")
-							.text( "World " + (i+1) )
-						.end()
-						.find(".expedTime")
-							.remove()
-						.end();
-
-					$(x).prepend(row);
-				}).on("click", '.expedNum input', function(){
-					var
-						worldNum     = Math.qckInt("ceil",($(this).attr("value")) / 8),
-						context      = ".tab_expedscorer .expedNumBox_"+worldNum,
-						parentCheck  = true;
-					self.exped_filters = [];
-					$(".expedNum   input",context).each(function(i,x){ parentCheck &= $(x).prop("checked"); });
-					$(".expedWhole input",context).prop("checked",parentCheck);
-				}).on("click", ".expedWhole input", function() {
-					var
-						worldNum = $(this).val(),
-						state    = $(this).prop("checked"),
-						expeds   = $(".tab_expedscorer .expedNumBox_"+worldNum+" .expedNum input");
-					expeds.each(function(i,x){
-						var
-							elmState = $(x).prop("checked"),
-							expedNum = parseInt($(x).val());
-						if(elmState ^ state) { // check different state
-							$(x).prop("checked",state);
-						}
-					});
-				});
-
-			// Calculate
-			var resultTable = $('.tab_expedscorer .results tbody');
-			$('.tab_expedscorer .calculate_btn').click(function(){
-				var fP = parseInt($(".tab_expedscorer .priorityFuel").val(), 10);
-				var aP = parseInt($(".tab_expedscorer .priorityAmmo").val(), 10);
-				var sP = parseInt($(".tab_expedscorer .prioritySteel").val(), 10);
-				var bP = parseInt($(".tab_expedscorer .priorityBaux").val(), 10);
-				var afkHH = parseInt($(".tab_expedscorer .afkH").val(), 10);
-				var afkMM = parseInt($(".tab_expedscorer .afkM").val(), 10);
-				var afkTime = afkHH*60 + afkMM;
-				var fleetCount = parseInt( $(".tab_expedscorer .fleetCounts input:checked").val(), 10);
-
-				let ExpedInfo = PS["KanColle.Expedition.New.Info"];
-				var selectedItemsQ = $('.tab_expedscorer .expedNumBox .expedNum input:checked');
-				var selectedItems = [];
-				selectedItemsQ.each( function() {
-					selectedItems.push( parseInt( $(this).attr("value"),10) );
-				});
-
-				// make sure we reload config every time
-				// so user can also have expedtable opened and whatever changes made there
-				// gets used immediately here.
-				let config = Expedition.loadExpedConfig(true);
-
-				// only compute income table for selected elements
-				let infoTable = {};
-				selectedItems.map( (eId) => {
-					let info = ExpedInfo.getInformation(eId);
-					infoTable[eId] = Expedition.computeNetIncome(config[eId],eId);
-					infoTable[eId].timeInMin = Math.max(afkTime, info.timeInMin);
-				});
-
-				// mark score on income
-				function markScore(income) {
-					income.score = income.fuel*fP + income.ammo*aP + income.steel*sP + income.bauxite*bP;
-					return income.score;
-				}
-
-				let results = Expedition
-					.chooseN(selectedItems, fleetCount)
-					.map( function( expedSet ) {
-						let retVal = {
-							set: expedSet,
-							fuel: 0, ammo: 0, steel: 0, bauxite: 0
-						};
-						expedSet.map( function (eId) {
-							let info = infoTable[eId];
-							["fuel","ammo","steel","bauxite"].map( function(resourceName) {
-								retVal[resourceName] += info[resourceName] * 60 / info.timeInMin;
-							});
-						});
-						markScore(retVal);
-						return retVal;
-					})
-					.sort( (a,b) => b.score - a.score )
-					.slice(0,50); // take only top 50
-
-				resultTable.empty();
-
-				for (let i = 0; i < results.length && i < 50; ++i) {
-					let curVal = results[i];
-					let row = $('<tr></tr>');
-					$('<td></td>').text(curVal.set.join(",")).appendTo(row);
-					$('<td></td>').text(curVal.fuel.toFixed(2)).appendTo(row);
-					$('<td></td>').text(curVal.ammo.toFixed(2)).appendTo(row);
-					$('<td></td>').text(curVal.steel.toFixed(2)).appendTo(row);
-					$('<td></td>').text(curVal.bauxite.toFixed(2)).appendTo(row);
-					$('<td></td>').text(curVal.score.toFixed(2)).appendTo(row);
-					resultTable.append( row );
-				}
-			});
+			this.setupControls();
 		}
-
 	};
 
 })();
