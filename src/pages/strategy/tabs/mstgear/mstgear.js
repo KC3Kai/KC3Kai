66a5ce1d1ae8939f575a1df91778a8fce6b3a24d
sin_sin <sinsinpub@gmail.com>
2017-02-11 23:28:41 +0800

    Fix gear range enum for new equipment
    Use `XL` for greater than range `VL` temporarily
    NOTE: [573] abyssal seaplane is first gear found `api_leng: 5`

--- src/pages/strategy/tabs/mstgear/mstgear.js
+++ src/pages/strategy/tabs/mstgear/mstgear.js
@@ -160,10 +160,12 @@
 					$("img", statBox).attr("src", "../../../../assets/img/stats/"+sdata[0]+".png");
 					if(sdata[0]==="rn"){ // For range length
 						$(".stat_value", statBox).text( [
-							"", "S", "M", "L", "VL"
-						][gearData["api_"+sdata[1]]] );
-					}else if(sdata[0]==="sp"){ // For speed
-						$(".stat_value", statBox).text( gearData["api_"+sdata[1]]==10?"F":"S" );
+							"?", "S", "M", "L", "VL", "XL"
+						][gearData["api_"+sdata[1]]] || "?" );
+					}else if(sdata[0]==="sp"){ // For speed, but not found in gears
+						$(".stat_value", statBox).text( ({
+							"0":"L", "5":"S", "10":"F", "15":"F+", "20":"F++"
+						})[gearData["api_"+sdata[1]]] || "?");
 					}else if(sdata[0]==="kk"){ // For bauxite cost when deploy to LBAS
 						var landSlot = KC3GearManager.landBaseReconnType2Ids.indexOf(gearData.api_type[2])>-1 ?
 							KC3GearManager.landBaseReconnMaxSlot : KC3GearManager.landBaseOtherMaxSlot;
