a11a6b4d1d6bb1f256dfac1c1ee3a2b63bb37259
107nationalstation <yyclxx123@163.com>
2017-03-05 18:01:10 +0800

    simple UI

--- src/pages/strategy/tabs/shipdrop/shipdrop.js
+++ src/pages/strategy/tabs/shipdrop/shipdrop.js
@@ -3,53 +3,36 @@
 
 	KC3StrategyTabs.shipdrop = new KC3StrategyTab("shipdrop");
 
-	KC3StrategyTabs.shipdrop.definition = {
-		tabSelf: KC3StrategyTabs.shipdrop,
-
-		/* INIT: mandatory
-		Prepares initial static data needed.
-		---------------------------------*/
-		init: function() {
-		},
-
-		/* RELOAD: optional
-		Loads latest player or game data if needed.
-		---------------------------------*/
-		reload: function() {
-		},
-
-		/* EXECUTE: mandatory
-		Places data onto the interface from scratch.
-		---------------------------------*/
-
-		execute: function() {
-			let contentRoot = $(".tab_shipdrop .content_root");
-			let factory = $(".tab_shipdrop .factory");
-			let hq = PlayerManager.hq.id;
-			let world = 37;
-			let subMap = 3;
-			let sorties37_1 = KC3Database.con.sortie.where("world").equals(world).and( data => data.mapnum === subMap && data.hq === hq);
-			let dropTable = {};
-			let pList = [];
-			//first: get info from KC3Database and count the drop of different node
-			sorties37_1.each( function(sortie) {
-				let p = KC3Database.con.battle.where("sortie_id").equals(sortie.id).each( function(battle) {
-					if (typeof dropTable[battle.node] === "undefined"){
-						dropTable[battle.node] = {};
-					}
-					let tbl = dropTable[battle.node];
-					if (typeof tbl[battle.drop] === "undefined") {
-						tbl[battle.drop] = 0;
-					}
-					++ tbl[battle.drop];
-				});
-				pList.push(p);
-			}).then( function() {
-				
-				Promise.all(pList).then( function() {
+	var exexe = {
+		show_all : false ,
+		world  :  0 ,
+		subMap :  0 ,
+		exe : function(){
+				console.log("exexe" , this.world , this.subMap);
+				let contentRoot = $(".tab_shipdrop .content_root");
+				let factory = $(".tab_shipdrop .factory");
+				let hq = PlayerManager.hq.id;
+				let sorties37_1 = KC3Database.con.sortie.where("world").equals(this.world).and( data => data.mapnum === this.subMap && data.hq === hq);
+				let dropTable = {};
+				let pList = [];
+				var self = this;
+				sorties37_1.each( function(sortie) {
+					let p = KC3Database.con.battle.where("sortie_id").equals(sortie.id).each( function(battle) {
+						if (typeof dropTable[battle.node] === "undefined"){
+							dropTable[battle.node] = {};
+						}
+						let tbl = dropTable[battle.node];
+						if (typeof tbl[battle.drop] === "undefined") {
+							tbl[battle.drop] = 0;
+						}
+						++ tbl[battle.drop];
+					});
+					pList.push(p);
+				}).then( function() {
+					Promise.all(pList).then( function() {
 					let node_tot = {};
 					$.each( dropTable, function(nodeNum, dropInfo) {
-						let node = KC3Meta.nodeLetter(world,subMap,nodeNum);
+						let node = KC3Meta.nodeLetter(self.world,self.subMap,nodeNum);
 						if(typeof node_tot[node] === "undefined") node_tot[node] = {};
 
 						let keys = Object.keys( dropInfo );
@@ -94,23 +77,9 @@
 					});
 				});
 			});
-
-		},
-
-		/* UPDATE: optional
-		Partially update elements of the interface,
-			possibly without clearing all contents first.
-		Be careful! Do not only update new data,
-			but also handle the old states (do cleanup).
-		Return `false` if updating all needed,
-			EXECUTE will be invoked instead.
-		---------------------------------*/
-		update: function(pageParams) {
-			// Use `pageParams` for latest page hash values,
-			// KC3StrategyTabs.pageParams keeps the old values for states tracking
-
-			// Returning `true` means updating has been handled.
-			return false;
 		}
-	};
+	}
+
+	
+	KC3StrategyTabs.shipdrop.definition = new KC3SortieLogs("shipdrop" , exexe);
 })();
