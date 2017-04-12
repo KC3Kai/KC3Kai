274f5ff596401bf603cdd9108e69caa4b0974ba5
sin_sin <sinsinpub@gmail.com>
2017-04-12 12:20:07 +0800

    Solve #1967 Color regen & hard cap for resources & consumables
    Also show left time to regen cap on tooltip of resources

--- src/pages/devtools/themes/natsuiro/natsuiro.js
+++ src/pages/devtools/themes/natsuiro/natsuiro.js
@@ -937,16 +937,48 @@
 		},
 
 		Consumables: function(data){
-			$(".count_fcoin").text( PlayerManager.consumables.fcoin || 0 );
-			$(".count_buckets").text( PlayerManager.consumables.buckets || 0 );
-			$(".count_screws").text( PlayerManager.consumables.screws || 0 );
-			$(".count_torch").text( PlayerManager.consumables.torch || 0 );
-			$(".count_devmats").text( PlayerManager.consumables.devmats || 0 );
+			$(".count_fcoin").text( PlayerManager.consumables.fcoin || 0 )
+				.toggleClass("hardCap", PlayerManager.consumables.fcoin >= 200000);
+			$(".count_buckets").text( PlayerManager.consumables.buckets || 0 )
+				.toggleClass("hardCap", PlayerManager.consumables.buckets >= PlayerManager.maxConsumable);
+			$(".count_screws").text( PlayerManager.consumables.screws || 0 )
+				.toggleClass("hardCap", PlayerManager.consumables.screws >= PlayerManager.maxConsumable);
+			$(".count_torch").text( PlayerManager.consumables.torch || 0 )
+				.toggleClass("hardCap", PlayerManager.consumables.torch >= PlayerManager.maxConsumable);
+			$(".count_devmats").text( PlayerManager.consumables.devmats || 0 )
+				.toggleClass("hardCap", PlayerManager.consumables.devmats >= PlayerManager.maxConsumable);
 			if(!!PlayerManager.hq.lastMaterial){
-				$(".count_fuel").text( PlayerManager.hq.lastMaterial[0] );
-				$(".count_steel").text( PlayerManager.hq.lastMaterial[2] );
-				$(".count_ammo").text( PlayerManager.hq.lastMaterial[1] );
-				$(".count_bauxite").text( PlayerManager.hq.lastMaterial[3] );
+				// Regen for fuel, ammo, steel: +3 every 3 minutes. bauxite +1 / 3mins
+				let roundUpTo3Mins = function(m) { return String(60 * (m + (m % 3 ? 3 - m % 3 : 0))); };
+				let regenCap = PlayerManager.hq.getRegenCap();
+				let fuel = PlayerManager.hq.lastMaterial[0],
+					ammo = PlayerManager.hq.lastMaterial[1],
+					steel = PlayerManager.hq.lastMaterial[2],
+					bauxite = PlayerManager.hq.lastMaterial[3];
+				$(".count_fuel").text( fuel )
+					.toggleClass("regenCap", fuel >= regenCap)
+					.toggleClass("hardCap", fuel >= PlayerManager.maxResource)
+					.attr("title",fuel >= regenCap ? "\u27A4" + regenCap :
+						"{0} \u27A4{1}".format(roundUpTo3Mins(regenCap - fuel).toHHMMSS(), regenCap))
+					.lazyInitTooltip();
+				$(".count_steel").text( steel )
+					.toggleClass("regenCap", steel >= regenCap)
+					.toggleClass("hardCap", steel >= PlayerManager.maxResource)
+					.attr("title", steel >= regenCap ? "\u27A4" + regenCap :
+						"{0} \u27A4{1}".format(roundUpTo3Mins(regenCap - steel).toHHMMSS(), regenCap))
+					.lazyInitTooltip();
+				$(".count_ammo").text( ammo )
+					.toggleClass("regenCap", ammo >= regenCap)
+					.toggleClass("hardCap", ammo >= PlayerManager.maxResource)
+					.attr("title", ammo >= regenCap ? "\u27A4" + regenCap :
+						"{0} \u27A4{1}".format(roundUpTo3Mins(regenCap - ammo).toHHMMSS(), regenCap))
+					.lazyInitTooltip();
+				$(".count_bauxite").text( bauxite )
+					.toggleClass("regenCap", bauxite >= regenCap)
+					.toggleClass("hardCap", bauxite >= PlayerManager.maxResource)
+					.attr("title", bauxite >= regenCap ? "\u27A4" + regenCap :
+						"{0} \u27A4{1}".format(String(180 * (regenCap - bauxite)).toHHMMSS(), regenCap))
+					.lazyInitTooltip();
 			}
 			// More pages could be added, see `api_get_member/useitem` in Kcsapi.js
 			$(".count_1classMedals").text( PlayerManager.consumables.firstClassMedals || 0 )
