e31f59c7dae86c52e75d32e653fe873b73883888
Adrymne <jealousy.of.tomorrow@gmail.com>
2017-04-24 22:30:03 +1000

    Move Akashi Repair tooltip
    Previously the tooltip for ship HP bars on the devtool fleet would
    overlap the Akashi timer brought up on the HP bar itself. The tooltip
    has been moved down, making the timer easier to read.

--- src/pages/devtools/themes/natsuiro/natsuiro.js
+++ src/pages/devtools/themes/natsuiro/natsuiro.js
@@ -53,7 +53,7 @@
 		// A lazy initialzing method, prevent duplicate tooltip instance
 		$.fn.lazyInitTooltip = function(opts) {
 			if(typeof this.tooltip("instance") === "undefined") {
-				this.tooltip(opts || nativeTooltipOptions);
+				this.tooltip($.extend(true, {}, nativeTooltipOptions, opts));
 			}
 			return this;
 		};
