5979dc5735b53dac04d9485af47aa0f50cdc4853
sin_sin <sinsinpub@gmail.com>
2017-05-03 00:16:35 +0800

    Update for new stuffs:
    * Update seasonal CG data
    * Update non-boss abyssal ships IDs
    * Enable ship type 1 for Ship List
    * Add new image of plane 228

--- src/pages/strategy/tabs/ships/ships.js
+++ src/pages/strategy/tabs/ships/ships.js
@@ -201,8 +201,9 @@
 			var sCtr, cElm;
 
 			for(sCtr in KC3Meta._stype){
-				// stype 1, 12, 15 not used by shipgirl
-				if(KC3Meta._stype[sCtr] && ["1", "12", "15"].indexOf(sCtr) < 0){
+				// stype 12, 15 not used by shipgirl
+				// stype 1 is used from 2017-05-02
+				if(KC3Meta._stype[sCtr] && ["12", "15"].indexOf(sCtr) < 0){
 					cElm = $(".tab_ships .factory .ship_filter_type").clone().appendTo(".tab_ships .filters .ship_types");
 					cElm.data("id", sCtr);
 					$(".filter_name", cElm).text(KC3Meta.stype(sCtr));
@@ -472,7 +473,7 @@
 			var stypes = Object
 				.keys(KC3Meta._stype)
 				.map(function(x) { return parseInt(x,10); })
-				.filter(function(x) { return [1,12,15].indexOf(x)<0; })
+				.filter(function(x) { return [12,15].indexOf(x)<0; })
 				.sort(function(a,b) { return a-b; });
 			console.assert(stypes[0] === 0);
 			// remove initial "0", which is invalid
