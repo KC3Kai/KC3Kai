8676cb8af74df4b39985857400ff2b4959ad2713
sin_sin <sinsinpub@gmail.com>
2017-09-29 12:19:24 +0800

    Fix grunt task globbing path
    Since we already INCLUDE 'assets/img/**' which `**` means matching all
    subdirs, its subdirs: `useitems/pay*`, `shipseasonal/**` are already included.
    Prepend `!` to them only if we wanna exclude them.
    
    * `useitems/pay*.png` are unused for now, so excluded.
    * `shipseasonal` folder, there is another task to copy them by json config,
    so always exclude it for this task.
    
    about glob pattern, see https://gruntjs.com/configuring-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,9 +43,8 @@ module.exports = function(grunt) {
 				cwd: 'build/tmp/',
 				src: [
 					'assets/img/**',
-					'assets/img/planes/**',
-					'assets/img/useitems/pay*',
-					'assets/img/shipseasonal/**',
+					'!assets/img/useitems/pay*',
+					'!assets/img/shipseasonal/**',
 					'assets/snd/**',
 					'assets/swf/**',
 					'assets/js/Chart.min.js',
