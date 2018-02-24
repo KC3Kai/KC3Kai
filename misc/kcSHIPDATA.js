67600d4b98e7d3376bffce395d4b82be7b82270e
sin_sin <sinsinpub@gmail.com>
2018-02-24 19:10:41 +0800

    Update abyssal stats, TL pointer

--- misc/kcSHIPDATA.js
+++ misc/kcSHIPDATA.js
@@ -5918,6 +5918,7 @@ var SHIPDATA = {
 		unknownstats: true,
 		RNG: 2,
 		SPD: 5,
+		SLOTS: [28, 28, 28, 0],
 		EQUIPS: [581, 582, 583, 580],
 	},
 	1787: {
@@ -5937,6 +5938,7 @@ var SHIPDATA = {
 		unknownstats: true,
 		RNG: 2,
 		SPD: 5,
+		SLOTS: [42, 35, 35, 0],
 		EQUIPS: [581, 582, 583, 580],
 	},
 	1788: {
@@ -5956,6 +5958,7 @@ var SHIPDATA = {
 		unknownstats: true,
 		RNG: 2,
 		SPD: 5,
+		SLOTS: [42, 35, 35, 0],
 		EQUIPS: [581, 582, 583, 580],
 	},
 	1789: {
