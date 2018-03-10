5828012f9f2240543689cab8ff93c57f62bbfaf6
FOG_Yamato <contact@cybersnets.com>
2018-03-10 20:20:45 +0200

    Changed TsunDB submission URL and Dev Info

--- src/library/modules/TsunDBSubmission.js
+++ src/library/modules/TsunDBSubmission.js
@@ -176,7 +176,7 @@
 		sendData: function(payload) {
 			//console.debug(JSON.stringify(payload));
 			$.ajax({
-				url: "http://kckai.cybersnets.com/api/routing",
+				url: "http://tsundb.kc3.moe/api/routing",
 				method: "POST",
 				headers: {"content-type": "application/json"},
 				data: JSON.stringify(payload)
