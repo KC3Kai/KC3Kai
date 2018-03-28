876130af0ffb29ce7f7e84721e80efe62a50b06d
sin_sin <sinsinpub@gmail.com>
2018-03-28 09:54:04 +0800

    Add auto DPR to screenshot setting
    Update TL pointer

--- src/library/objects/Screenshot.js
+++ src/library/objects/Screenshot.js
@@ -3,7 +3,7 @@ var enableShelfTimer = false;
 
 function KCScreenshot(){
 	ConfigManager.load();
-	this.scale = ((ConfigManager.api_gameScale || 100) / 100) * (ConfigManager.ss_dppx || 1);
+	this.scale = ((ConfigManager.api_gameScale || 100) / 100) * (ConfigManager.ss_dppx || window.devicePixelRatio || 1);
 	this.gamebox = {};
 	this.canvas = {};
 	this.context = {};
