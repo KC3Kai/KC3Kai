70bcc1f251d7841463f1805da63728aa0ae445b8
jacky2001114 <jacky2001114@gmail.com>
2018-04-23 00:16:54 +0800

    fix quest data lost

--- src/pages/devtools/themes/default/default.js
+++ src/pages/devtools/themes/default/default.js
@@ -46,7 +46,17 @@
 		KC3Network.addGlobalListener(function(event, data){
 			if(KC3Panel.state == "running"){
 				KC3Panel.layout().trigger(event, data);
-				localStorage.quests = '{"id":0,"type":0,"status":0,"progress":0,"materials":[0,0,0,0],"tracking":false}'
+				
+				var tempQuests = JSON.parse(localStorage.quests);			
+				var tempQuest;			
+				var questArray = [];
+				
+				for(var ctr in tempQuests){
+					tempQuest = tempQuests[ctr];
+					tempQuest.status=1
+					questArray.push(tempQuest)
+				}
+				localStorage.quests = JSON.stringify(questArray);
 			}
 		});
 		
