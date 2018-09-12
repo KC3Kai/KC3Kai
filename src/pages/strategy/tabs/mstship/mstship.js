80b360d87109201b5d654b94b985545baa6dbae6
sin_sin <sinsinpub@gmail.com>
2018-09-13 01:23:07 +0800

    Fix to not show some nonexistent seasonal CGs

--- src/pages/strategy/tabs/mstship/mstship.js
+++ src/pages/strategy/tabs/mstship/mstship.js
@@ -431,7 +431,15 @@
 			if(showAllGraphs){
 				$(".tab_mstship .shipInfo .cgswf").hide();
 				const cgList = $('<div class="cglist"></div>').appendTo(".tab_mstship .shipInfo .basic");
-				const availableTypes = KC3Master.isSeasonalShip(ship_id) ? [
+				// To avoid loading seasonal image types not existed,
+				// see `ShipLoader.prototype.getSpecificAlbumImageLoadList`
+				const isSpecificAlbumTypes = [754, 755, 984, 1003, 1004,
+					1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013,
+					1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022,
+					1023, 1024, 1025, 1026, 1027, 1028, 1029, 1030, 1031,
+					1032, 1033, 1034].includes(ship_id);
+				const availableTypes = KC3Master.isSeasonalShip(ship_id) ?
+					isSpecificAlbumTypes ? ['character_full', 'character_up'] : [
 						'card', 'character_full', 'character_full_dmg',
 						'character_up', 'character_up_dmg'
 					] : KC3Master.isAbyssalShip(ship_id) ? [
