2b6f1d6257ed4c6d210ef1f39ee760fd48463611
sin_sin <sinsinpub@gmail.com>
2018-10-17 18:25:17 +0800

    Avoid NaN percentage on exped history page
    * chore clean for TsunDBSubmission

--- src/library/modules/TsunDBSubmission.js
+++ src/library/modules/TsunDBSubmission.js
@@ -420,16 +420,16 @@
 						const squadronPlanes = koukuApi.api_map_squadron_plane[baseId] || [];
 						const shipObj = buildShipFromBase(baseInfo, squadronPlanes);
 						const planes = squadronPlanes.map(plane => plane.api_mst_id || -1);
-						const proficiency = shipObj.equipment().map( g => g.ace );
+						const proficiency = shipObj.equipment().map(g => g.ace);
 						airBattle.planes.push(planes);
 						airBattle.slots.push(shipObj.slots);
 						airBattle.proficiency.push(proficiency);
 						fp += shipObj.interceptionPower();
 						
 						if(koukuApi.api_stage3) {
-							airBattle.bakFlag = (koukuApi.api_stage3.api_fbak_flag || []);
-							airBattle.raiFlag = (koukuApi.api_stage3.api_frai_flag || []);
-							airBattle.fclFlag = (koukuApi.api_stage3.api_fcl_flag || []);
+							airBattle.bakFlag = koukuApi.api_stage3.api_fbak_flag || [];
+							airBattle.raiFlag = koukuApi.api_stage3.api_frai_flag || [];
+							airBattle.fclFlag = koukuApi.api_stage3.api_fcl_flag || [];
 							airBattle.damage = koukuApi.api_stage3.api_fdam;
 						}
 					});
