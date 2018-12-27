4659cf44190696119a439107adc0d8b578218042
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2018-12-27 09:49:33 +0100

    Gauge_num and type API location changed

--- src/library/modules/TsunDBSubmission.js
+++ src/library/modules/TsunDBSubmission.js
@@ -316,8 +316,8 @@
 				this.data.currentMapHP = apiData.api_eventmap.api_now_maphp;
 				this.data.maxMapHP = apiData.api_eventmap.api_max_maphp;
 				this.data.difficulty = mapData.api_eventmap.api_selected_rank;
-				this.data.gaugeNum = mapData.api_eventmap.api_gauge_num || 1;
-				this.data.gaugeType = mapData.api_eventmap.api_gauge_type;
+				this.data.gaugeNum = mapData.api_gauge_num || 1;
+				this.data.gaugeType = mapData.api_gauge_type;
 				this.data.debuffSound = mapStorage.debuffSound;
 				
 				this.sendData(this.data, 'eventrouting');
