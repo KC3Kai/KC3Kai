d826d10fb0af490aeb908f7a673a004e6c08f75c
sin_sin <sinsinpub@gmail.com>
2019-05-01 13:37:01 +0800

    Update visible range bonus

--- src/library/objects/Gear.js
+++ src/library/objects/Gear.js
@@ -50,7 +50,7 @@ KC3改 Equipment Object
 	 * @see https://wikiwiki.jp/kancolle/%E8%A3%85%E5%82%99#bonus - about naming of this bonus type
 	 * @see https://kancolle.fandom.com/wiki/Equipment_Bonuses - summary tables and named: visible bonuses
 	 * @see URLs some other summary tables:
-	 *  * [20190423 ALL] https://docs.google.com/spreadsheets/d/1bInH11S_xKdaKP754bB7SYh-di9gGzcXkiQPvGuzCpg/htmlview
+	 *  * [20190430 ALL] https://docs.google.com/spreadsheets/d/1bInH11S_xKdaKP754bB7SYh-di9gGzcXkiQPvGuzCpg/htmlview
 	 *  * [20190208 ALL] https://docs.google.com/spreadsheets/d/1_peG-B4ijt7HOvDtkd8dPZ8vA7ZMLx-YuwsuGoEm6wY/htmlview
 	 *  * [20180904 ALL] https://github.com/andanteyk/ElectronicObserver/blob/develop/ElectronicObserver/Other/Information/kcmemo.md#%E7%89%B9%E6%AE%8A%E8%A3%85%E5%82%99%E3%81%AB%E3%82%88%E3%82%8B%E3%83%91%E3%83%A9%E3%83%A1%E3%83%BC%E3%82%BF%E8%A3%9C%E6%AD%A3
 	 *  * [20180816 ALL] http://furukore.com/archives/13793
@@ -347,6 +347,13 @@ KC3改 Equipment Object
 						remodel: 2,
 						multiple: { "houg": 6, "houk": 1 },
 					},
+					// Souryuu Kai Ni range +2
+					"17": {
+						remodel: 2,
+						single: { "leng": 1 },
+					},
+					// Hiryuu Kai Ni range +2
+					"25": 17,
 				},
 			},
 			// Suisei Model 22 (634 Air Group / Skilled)
@@ -359,6 +366,13 @@ KC3改 Equipment Object
 						remodel: 2,
 						multiple: { "houg": 8, "tyku": 1, "houk": 2 },
 					},
+					// Souryuu Kai Ni range +2
+					"17": {
+						remodel: 2,
+						single: { "leng": 1 },
+					},
+					// Hiryuu Kai Ni range +2
+					"25": 17,
 				},
 			},
 			// Suisei Model 12 (634 Air Group w/Type 3 Cluster Bombs)
@@ -447,6 +461,13 @@ KC3改 Equipment Object
 						remodel: 2,
 						single: { "houg": 3, "souk": 1, "houk": 2, "leng": 1 },
 					},
+					// Souryuu Kai Ni range +1
+					"17": {
+						remodel: 2,
+						single: { "leng": 1 },
+					},
+					// Hiryuu Kai Ni range +1
+					"25": 17,
 				},
 				byShip: [
 					{
