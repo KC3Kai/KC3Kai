a636da440660c69c100c60f2181b8f3cbb9b5329
sin_sin <sinsinpub@gmail.com>
2019-05-21 12:41:28 +0800

    Update WhoCallsTheFleet DB

--- src/library/modules/WhoCallsTheFleetDb.js
+++ src/library/modules/WhoCallsTheFleetDb.js
@@ -3,8 +3,8 @@
 
 	window.WhoCallsTheFleetDb = {
 		db: {},
-		expectedShipCount: 560,
-		expectedItemCount: 329,
+		expectedShipCount: 568,
+		expectedItemCount: 338,
 		init: function(repo) {
 			var self = this;
 			var loadAndParseDb = function(prefix, filename, expectedCount) {
