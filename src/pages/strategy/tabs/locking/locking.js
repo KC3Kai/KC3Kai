ac78e2c03899599ebbea31f6836a65c925b67c87
sin_sin <sinsinpub@gmail.com>
2019-05-21 12:42:42 +0800

    Updates for event maint:
    * Add Spring 2019 event tab
    * Update fud_quarterly.json (boss nodes missing)
    * Update locking page
    * Update seasonal mstship
    * Update remodel info for Akagi K2/K2E
    * Update texture constants for mapmakers

--- src/pages/strategy/tabs/locking/locking.js
+++ src/pages/strategy/tabs/locking/locking.js
@@ -12,7 +12,7 @@
         init() {
             this.defineSorters();
             this.showListRowCallback = this.showShipLockingRow;
-            this.lockLimit = 4;
+            this.lockLimit = 3;
             this.heartLockMode = 2;
             this.showShipLevel = true;
         }
