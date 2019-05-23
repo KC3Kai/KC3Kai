6020cbf528e5f3924da78bbfa6f113b58fc3c5f5
sin_sin <sinsinpub@gmail.com>
2019-05-23 10:45:47 +0800

    All langs use smaller font for weekdays

--- src/pages/strategy/tabs/gears/gears.js
+++ src/pages/strategy/tabs/gears/gears.js
@@ -510,14 +510,12 @@
 
 				["sun", "mon", "tue", "wed", "thu", "fri", "sat"].forEach((day, dayIndex) => {
 					if (self.upgrades[day] && Array.isArray(self.upgrades[day][ThisSlotitem.id])) {
-						const dayName = Date.getDayName(dayIndex);
-						$("<a></a>").text(dayName)
+						$("<a></a>").text(Date.getDayName(dayIndex))
 							.attr("title",
 								self.upgrades[day][ThisSlotitem.id].map(shipId =>
 									KC3Meta.shipName(KC3Master.ship(shipId).api_name)
 								).join(", ")
 							).attr("href", `#akashi-${day}`)
-							.css("font-size", dayName.length > 1 ? "smaller" : "")
 							.toggleClass("sel", dayIndex === dayOfWeek)
 							.appendTo($(".upgradeDays", ItemElem));
 					}
