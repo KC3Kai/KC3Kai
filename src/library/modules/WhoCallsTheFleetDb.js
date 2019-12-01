73ff170ea6b1331b5fa890c30c0f53bf8ad139d5
sin_sin <sinsinpub@gmail.com>
2019-12-01 10:16:14 +0800

    Update WhoCallsTheFleet DB, and:
    * Update boss edges
    * Update locking page to 6
    * Update TL pointer

--- src/library/modules/WhoCallsTheFleetDb.js
+++ src/library/modules/WhoCallsTheFleetDb.js
@@ -3,8 +3,8 @@
 
 	window.WhoCallsTheFleetDb = {
 		db: {},
-		expectedShipCount: 582,
-		expectedItemCount: 356,
+		expectedShipCount: 590,
+		expectedItemCount: 358,
 		init: function(repo) {
 			var self = this;
 			var loadAndParseDb = function(prefix, filename, expectedCount) {
