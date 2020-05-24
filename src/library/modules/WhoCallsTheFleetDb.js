30bd65c67178ca7db939eba6d3e27b973c2dee67
sin_sin <sinsinpub@gmail.com>
2020-05-24 10:50:39 +0800

    Update WhoCallsTheFleet DB, WiP akashi.json

--- src/library/modules/WhoCallsTheFleetDb.js
+++ src/library/modules/WhoCallsTheFleetDb.js
@@ -3,8 +3,8 @@
 
 	window.WhoCallsTheFleetDb = {
 		db: {},
-		expectedShipCount: 603,
-		expectedItemCount: 371,
+		expectedShipCount: 605,
+		expectedItemCount: 375,
 		init: function(repo) {
 			var self = this;
 			var loadAndParseDb = function(prefix, filename, expectedCount) {
