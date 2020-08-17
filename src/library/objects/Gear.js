94cc42b631aea0b3cc53a21dde5905e1455dad5a
sin_sin <sinsinpub@gmail.com>
2020-08-17 13:07:44 +0800

    Update akashi.json
    * Add some missing items for Hiei K2C

--- src/library/objects/Gear.js
+++ src/library/objects/Gear.js
@@ -4531,15 +4531,15 @@ KC3改 Equipment Object
 			return 0;
 		}
 		switch (type2) {
-			case 6: // carrier-based fighter
-			case 45: // seaplane fighter. seaplane bomber no AA bonus found yet, but found DV & LoS bonus
+			case 6: // Carrier-based fighter
+			case 45: // Seaplane fighter. Seaplane bomber no AA bonus found yet, but found DV & LoS bonus
 			case 48: // LB fighter or LB interceptor
 				modifier = 0.2; break;
-			case 7: // fighter bomber (dive bomber with AA stat)
-			case 49: // LB recon
-			case 57: // jet bomber
+			case 7: // Fighter bomber (dive bomber with AA stat)
+			case 49: // LB recon, uncertain?
+			case 57: // Jet bomber
 				modifier = 0.25; break;
-			case 41: // Large Flying Boat
+			case 41: // Large Flying Boat, uncertain?
 				return 0.25 * Math.sqrt(stars);
 			case 47: // LB attacker
 				return 0.5 * Math.sqrt(stars);
