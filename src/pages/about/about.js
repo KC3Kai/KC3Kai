a8302828c04c0eadfb232d0599b91bc9e93c64c0
sin_sin <sinsinpub@gmail.com>
2020-08-18 14:07:51 +0800

    List patreon donators in seperated section

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -16,7 +16,7 @@
 		
 		// Load and show developer list
 		$.getJSON("../../data/developers.json", function(response){
-			var sectionBox, sectionName, devTypeCode, devCtr, devCatBox, devName;
+			var sectionBox, sectionName, devTypeCode, devCtr, devCatBox, devName, donator;
 			
 			console.log("response", response);
 			
@@ -50,6 +50,13 @@
 						}
 						$(".list", sectionBox).append($("<div/>").addClass("clear"));
 						break;
+					case "AboutDonators":
+						for (devCtr in response.AboutDonators) {
+							donator = response.AboutDonators[devCtr];
+							addDonator({ user: donator[0], amount: donator[1], legacy: true }, $(".list", sectionBox));
+						}
+						$(".list", sectionBox).append($("<div/>").addClass("clear"));
+						break;
 					default: break;
 				}
 			}
@@ -59,11 +66,13 @@
 		$.get("https://raw.githubusercontent.com/KC3Kai/KC3Kai/master/LICENSE", function(response){
 			$(".license_box").html("").append($("<pre>").text(response));
 		});
-
-		// Load donator list
+		
+		// Load patreon donator list
+		$("#patreonDonators").text("Loading...");
 		$.get("https://tsundb.kc3.moe/api/patrons", function(response){
-			for (var donator of response) {
-				addDonator( donator );
+			$("#patreonDonators").empty();
+			for (var donatorCtr in response) {
+				addDonator(response[donatorCtr], $("#patreonDonators"));
 			}
 		});
 	});
@@ -154,14 +163,13 @@
 	}
 	
 	// Show a donator box
-	function addDonator(data){
+	function addDonator(data, targetBox){
 		var donatorBox = $("#factory .donatorBox").clone();
 		$(".donatorName", donatorBox).text(data.user);
 		$(".donatorAmount", donatorBox).text(data.amount);
-		if(data.sponsor) {
-			donatorBox.addClass("sponsor");
-		}
-		$("#donatorWrapper").append(donatorBox);
+		donatorBox.toggleClass("sponsor", !!data.sponsor)
+			.toggleClass("legacy", !!data.legacy);
+		targetBox.append(donatorBox);
 	}
 	
 })();
