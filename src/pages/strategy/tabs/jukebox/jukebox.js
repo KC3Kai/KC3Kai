26fd3e2809c16583a56e7cf66d1a62b0785107fb
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-10-01 02:10:47 +0700

    Add semicolon

--- src/pages/strategy/tabs/jukebox/jukebox.js
+++ src/pages/strategy/tabs/jukebox/jukebox.js
@@ -37,102 +37,102 @@
     Places data onto the interface from scratch.
     ---------------------------------*/
     execute: function () {
-      this.loadConfig()
-      this.initAudioListeners()
+      this.loadConfig();
+      this.initAudioListeners();
 
       $('.type.port').on('click', () => {
-        this.selectType('port')
-      })
+        this.selectType('port');
+      });
       $('.type.battle').on('click', () => {
-        this.selectType('battle')
-      })
+        this.selectType('battle');
+      });
 
       // Init default list
-      this.selectType(this.type)
+      this.selectType(this.type);
     },
 
     getConfig() {
       const config = {
         type: this.getType(),
         volume: this.getVolume(),
-      }
-      return config
+      };
+      return config;
     },
 
     loadConfig() {
-      let config
+      let config;
       try {
-        config = JSON.parse(localStorage.getItem('jukebox'))
+        config = JSON.parse(localStorage.getItem('jukebox'));
       } catch (error) {
       }
       if (!config) {
-        config = this.getConfig()
-        this.saveConfig()
+        config = this.getConfig();
+        this.saveConfig();
       }
-      this.setType(config.type)
-      this.setVolume(config.volume)
-      return config
+      this.setType(config.type);
+      this.setVolume(config.volume);
+      return config;
     },
 
     saveConfig() {
-      const config = this.getConfig()
-      localStorage.setItem('jukebox', JSON.stringify(config))
+      const config = this.getConfig();
+      localStorage.setItem('jukebox', JSON.stringify(config));
     },
 
     getType() {
-      return this.type
+      return this.type;
     },
 
     setType(value) {
-      this.type = value ?? 'battle'
+      this.type = value ?? 'battle';
     },
 
     getVolume() {
-      return this.volume
+      return this.volume;
     },
 
     setVolume(value) {
-      this.volume = value ?? 0.1
+      this.volume = value ?? 0.1;
     },
 
     getAudio() {
-      const audio = document.querySelector('.music-player audio')
-      return audio
+      const audio = document.querySelector('.music-player audio');
+      return audio;
     },
 
     playAudio(src) {
-      const audio = this.getAudio()
-      audio.volume = this.getVolume()
-      audio.src = src
-      audio.play()
+      const audio = this.getAudio();
+      audio.volume = this.getVolume();
+      audio.src = src;
+      audio.play();
     },
 
     initAudioListeners() {
-      const audio = this.getAudio()
+      const audio = this.getAudio();
       audio.addEventListener('volumechange', () => {
-        this.setVolume(audio.volume)
-        this.saveConfig()
-      })
+        this.setVolume(audio.volume);
+        this.saveConfig();
+      });
     },
 
     clearList() {
-      $('.music-list').html('')
+      $('.music-list').html('');
     },
 
     selectType(type) {
-      this.type = type
-      this.saveConfig()
+      this.type = type;
+      this.saveConfig();
 
-      $('.music-types .type').removeClass('active')
-      $(`.music-types .type.${type}`).addClass('active')
-      this.clearList()
+      $('.music-types .type').removeClass('active');
+      $(`.music-types .type.${type}`).addClass('active');
+      this.clearList();
 
       switch (type) {
         case 'port':
-          this.loadPortBgms()
-          return
+          this.loadPortBgms();
+          return;
         default:
-          this.loadBattleBgms()
+          this.loadBattleBgms();
       }
     },
 
@@ -146,45 +146,45 @@
           .clone()
           .appendTo('.music-list')
           .attr('bgm-type', this.type)
-          .attr('bgm-id', bgm.api_id)
+          .attr('bgm-id', bgm.api_id);
 
-        $('.id', item).text('#' + String(bgm.api_id).pad(3, '0'))
-        $('.desc', item).text(bgm.api_name)
-      })
+        $('.id', item).text('#' + String(bgm.api_id).pad(3, '0'));
+        $('.desc', item).text(bgm.api_name);
+      });
 
-      this.initTrackListeners()
+      this.initTrackListeners();
     },
 
     loadPortBgms() {
-      $('.music-list').addClass('list')
-      this.loadBgms(Object.values(this.portbgm))
+      $('.music-list').addClass('list');
+      this.loadBgms(Object.values(this.portbgm));
     },
 
     loadBattleBgms() {
-      $('.music-list').removeClass('list')
+      $('.music-list').removeClass('list');
       const bgms = Array(this.maxBattleId)
         .fill(null)
         .map((_, i) => i + 1)
         .filter(id => !this.missingBattleIds.includes(id))
         .sort((a, b) => b - a)
-        .map(id => ({ api_id: id }))
-      this.loadBgms(bgms)
+        .map(id => ({ api_id: id }));
+      this.loadBgms(bgms);
     },
 
     initTrackListeners() {
       $('.music-list .track').click(ev => {
-        const target = ev.currentTarget
-        const id = $(target).attr('bgm-id')
-        const type = $(target).attr('bgm-type')
+        const target = ev.currentTarget;
+        const id = $(target).attr('bgm-id');
+        const type = $(target).attr('bgm-type');
         const src = `http://${this.serverIp}/kcs2/resources${KC3Master.bgm_file(id, type)}`;
-        this.playAudio(src)
+        this.playAudio(src);
 
-        $('.music-list .track').removeClass('active')
-        $(target).addClass('active')
+        $('.music-list .track').removeClass('active');
+        $(target).addClass('active');
 
-        $('.music-player .id').text($('.id', target).text())
-        $('.music-player .desc').text($('.desc', target).text())
-      })
+        $('.music-player .id').text($('.id', target).text());
+        $('.music-player .desc').text($('.desc', target).text());
+      });
     },
 
     /* UPDATE: optional
