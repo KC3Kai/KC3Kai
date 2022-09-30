223db312e699f5832e1498172627702814dd7000
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-09-30 23:34:59 +0700

    Update jukebox tab

--- src/pages/strategy/tabs/jukebox/jukebox.js
+++ src/pages/strategy/tabs/jukebox/jukebox.js
@@ -1,100 +1,204 @@
-(function(){
-	"use strict";
+(function () {
+  'use strict';
 
-	KC3StrategyTabs.jukebox = new KC3StrategyTab("jukebox");
+  KC3StrategyTabs.jukebox = new KC3StrategyTab('jukebox');
 
-	KC3StrategyTabs.jukebox.definition = {
-		tabSelf: KC3StrategyTabs.jukebox,
+  KC3StrategyTabs.jukebox.definition = {
+    tabSelf: KC3StrategyTabs.jukebox,
 
-		/* INIT: mandatory
-		Prepares initial static data needed.
-		---------------------------------*/
-		init: function() {
-			const myServer = (new KC3Server()).setNum(PlayerManager.hq.server);
-			this.serverIp = myServer.ip;
-		},
+    // Config
+    type: 'battle',
+    volume: 0.1,
 
-		/* RELOAD: optional
-		Loads latest player or game data if needed.
-		---------------------------------*/
-		reload: function() {
-			this.portbgm = KC3Master._raw.bgm;
-			this.mapbgm = KC3Master._raw.mapbgm;
-		},
+    // Battle BGM
+    maxBattleId: 215,
+    missingBattleIds: [24],
 
-		/* EXECUTE: mandatory
-		Places data onto the interface from scratch.
-		---------------------------------*/
-		execute: function() {
-			const audioElm = $(".player audio"), audio = audioElm.get(0);
-			audio.onplay = () => {
-				$(".player").addClass("playing");
-			};
-			audio.onended = () => {
-				$(".player").removeClass("playing");
-			};
-			audio.oncanplay = (e) => {
-				e.target.play();
-			};
-			let evenodd = false;
-			const addTrack = (bgmtype, bgmid, bgmdesc) => {
-				const item = $(".factory .track").clone().appendTo(".musiclist ." + bgmtype);
-				$(".id", item).text("#" + String(bgmid).pad(3, "0"))
-					.data("bgmid", bgmid).data("bgmtype", bgmtype);
-				$(".desc", item).text("\u266a " + (bgmdesc || "-"));
-				item.toggleClass("even", !!evenodd).toggleClass("odd", !evenodd);
-				return item;
-			};
-			$.each(this.portbgm, (idx, bgm) => {
-				addTrack("port", bgm.api_id, bgm.api_name);
-				evenodd = !evenodd;
-			});
-			evenodd = false;
-			let prevMap = 1;
-			$.each(this.mapbgm, (idx, bgm) => {
-				const world = bgm.api_maparea_id, map = bgm.api_no;
-				const mapName = "World {0}{1}".format(
-					KC3Meta.isEventWorld(world) ? world + " " : "",
-					KC3Meta.mapToDesc(world, map)
-				);
-				const trackClass = [
-					KC3Meta.isEventWorld(world) ? "event" : "world{0}".format(world),
-					"map{0}".format(map)
-				].join(" ");
-				if(map !== prevMap) evenodd = !evenodd;
-				addTrack("battle", bgm.api_moving_bgm, mapName + " Overworld").addClass(trackClass);
-				if(bgm.api_map_bgm[0]) addTrack("battle", bgm.api_map_bgm[0], mapName + " Day Battle").addClass(trackClass);
-				if(bgm.api_map_bgm[1]) addTrack("battle", bgm.api_map_bgm[1], mapName + " Night Battle").addClass(trackClass);
-				if(bgm.api_boss_bgm[0]) addTrack("battle", bgm.api_boss_bgm[0], mapName + " Day Boss").addClass(trackClass);
-				if(bgm.api_boss_bgm[1]) addTrack("battle", bgm.api_boss_bgm[1], mapName + " Night Boss").addClass(trackClass);
-				prevMap = map;
-			});
-			$(".musiclist .playbtn").addClass("hover").click((e) => {
-				audio.pause();
-				const item = $(e.target).parent();
-				const bgmId = $(".id", item).data("bgmid");
-				const bgmType = $(".id", item).data("bgmtype");
-				const bgmDesc = $(".desc", item).text();
-				const bgmSrc = `http://${this.serverIp}/kcs2/resources${KC3Master.bgm_file(bgmId, bgmType)}`;
-				$(".player .playing").text("#{0} {1}".format(bgmId, bgmDesc));
-				$(audio).attr("src", bgmSrc);
-			});
-		},
+    /* INIT: mandatory
+    Prepares initial static data needed.
+    ---------------------------------*/
+    init: function () {
+      const myServer = (new KC3Server()).setNum(PlayerManager.hq.server);
+      this.serverIp = myServer.ip;
+    },
 
-		/* UPDATE: optional
-		Partially update elements of the interface,
-			possibly without clearing all contents first.
-		Be careful! Do not only update new data,
-			but also handle the old states (do cleanup).
-		Return `false` if updating all needed,
-			EXECUTE will be invoked instead.
-		---------------------------------*/
-		update: function(pageParams) {
-			// Use `pageParams` for latest page hash values,
-			// KC3StrategyTabs.pageParams keeps the old values for states tracking
+    /* RELOAD: optional
+    Loads latest player or game data if needed.
+    ---------------------------------*/
+    reload: function () {
+      this.portbgm = KC3Master._raw.bgm;
+      this.mapbgm = KC3Master._raw.mapbgm;
+      this.loadConfig()
+    },
 
-			// Returning `true` means updating has been handled.
-			return false;
-		}
-	};
+    /* EXECUTE: mandatory
+    Places data onto the interface from scratch.
+    ---------------------------------*/
+    execute: function () {
+      this.loadConfig()
+      this.initAudioListeners()
+
+      $('.type.port').on('click', () => {
+        this.selectType('port')
+      })
+      $('.type.battle').on('click', () => {
+        this.selectType('battle')
+      })
+
+      // Init default list
+      this.selectType(this.type)
+    },
+
+    getConfig() {
+      const config = {
+        type: this.getType(),
+        volume: this.getVolume(),
+      }
+      return config
+    },
+
+    loadConfig() {
+      let config
+      try {
+        config = JSON.parse(localStorage.getItem('jukebox'))
+      } catch (error) {
+      }
+      if (!config) {
+        config = this.getConfig()
+        this.saveConfig()
+      }
+      this.setType(config.type)
+      this.setVolume(config.volume)
+      return config
+    },
+
+    saveConfig() {
+      const config = this.getConfig()
+      localStorage.setItem('jukebox', JSON.stringify(config))
+    },
+
+    getType() {
+      return this.type
+    },
+
+    setType(value) {
+      this.type = value ?? 'battle'
+    },
+
+    getVolume() {
+      return this.volume
+    },
+
+    setVolume(value) {
+      this.volume = value ?? 0.1
+    },
+
+    getAudio() {
+      const audio = document.querySelector('.music-player audio')
+      return audio
+    },
+
+    playAudio(src) {
+      const audio = this.getAudio()
+      audio.volume = this.getVolume()
+      audio.src = src
+      audio.play()
+    },
+
+    initAudioListeners() {
+      const audio = this.getAudio()
+      audio.addEventListener('volumechange', () => {
+        this.setVolume(audio.volume)
+        this.saveConfig()
+      })
+    },
+
+    clearList() {
+      $('.music-list').html('')
+    },
+
+    selectType(type) {
+      this.type = type
+      this.saveConfig()
+
+      $('.music-types .type').removeClass('active')
+      $(`.music-types .type.${type}`).addClass('active')
+      this.clearList()
+
+      switch (type) {
+        case 'port':
+          this.loadPortBgms()
+          return
+        default:
+          this.loadBattleBgms()
+      }
+    },
+
+    /**
+     *
+     * @param {*} bgms list of bgm, with `api_id` and `api_name`
+     */
+    loadBgms(bgms) {
+      bgms.forEach(bgm => {
+        const item = $('.factory .track')
+          .clone()
+          .appendTo('.music-list')
+          .attr('bgm-type', this.type)
+          .attr('bgm-id', bgm.api_id)
+
+        $('.id', item).text('#' + String(bgm.api_id).pad(3, '0'))
+        $('.desc', item).text(bgm.api_name)
+      })
+
+      this.initTrackListeners()
+    },
+
+    loadPortBgms() {
+      $('.music-list').addClass('list')
+      this.loadBgms(Object.values(this.portbgm))
+    },
+
+    loadBattleBgms() {
+      $('.music-list').removeClass('list')
+      const bgms = Array(this.maxBattleId)
+        .fill(null)
+        .map((_, i) => i + 1)
+        .filter(id => !this.missingBattleIds.includes(id))
+        .sort((a, b) => b - a)
+        .map(id => ({ api_id: id }))
+      this.loadBgms(bgms)
+    },
+
+    initTrackListeners() {
+      $('.music-list .track').click(ev => {
+        const target = ev.currentTarget
+        const id = $(target).attr('bgm-id')
+        const type = $(target).attr('bgm-type')
+        const src = `http://${this.serverIp}/kcs2/resources${KC3Master.bgm_file(id, type)}`;
+        this.playAudio(src)
+
+        $('.music-list .track').removeClass('active')
+        $(target).addClass('active')
+
+        $('.music-player .id').text($('.id', target).text())
+        $('.music-player .desc').text($('.desc', target).text())
+      })
+    },
+
+    /* UPDATE: optional
+    Partially update elements of the interface,
+      possibly without clearing all contents first.
+    Be careful! Do not only update new data,
+      but also handle the old states (do cleanup).
+    Return `false` if updating all needed,
+      EXECUTE will be invoked instead.
+    ---------------------------------*/
+    update: function (pageParams) {
+      // Use `pageParams` for latest page hash values,
+      // KC3StrategyTabs.pageParams keeps the old values for states tracking
+
+      // Returning `true` means updating has been handled.
+      return false;
+    }
+  };
 })();
