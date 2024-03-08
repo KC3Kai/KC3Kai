7b917e0cacbdc743d3daa0827b21c4b61dcf616a
sin_sin <sinsinpub@gmail.com>
2024-03-08 14:23:21 +0800

    Update WhoCallsTheFleet DB
    * Update WiP akashi.json
    * Misc fixes
    * Update TL pointer

--- src/library/objects/Fleet.js
+++ src/library/objects/Fleet.js
@@ -394,6 +394,7 @@ Contains summary information about a fleet and its ships
 		var normalCount = 0;
 		var t89Count = 0;
 		var t2Count = 0;
+		var t4Count = 0;
 		var tokuCount = 0;
 		var abCount = 0;
 		var armedCount = 0;
@@ -409,8 +410,6 @@ Contains summary information about a fleet and its ships
 				if (gearObj.isDummy()) return;
 				switch (gearObj.masterId) {
 					case 68: // normal landing craft
-					case 525: // T4 tank?
-					case 526: // T4 tank kai?
 						normalCount += 1;
 						addImprove(gearObj.stars);
 					break;
@@ -420,6 +419,11 @@ Contains summary information about a fleet and its ships
 					break;
 					case 167: // T2 tank
 						t2Count += 1;
+						addImprove(gearObj.stars);
+					break;
+					case 525: // T4 tank?
+					case 526: // T4 tank kai?
+						t4Count += 1;
 						addImprove(gearObj.stars);
 					break;
 					case 193: // Toku landing craft
@@ -453,7 +457,7 @@ Contains summary information about a fleet and its ships
 			});
 		});
 		// without cap
-		const basicBonus= 0.05 * (normalCount + tokuCount + bonusShipCount)
+		const basicBonus= 0.05 * (normalCount + tokuCount + t4Count + bonusShipCount)
 						+ 0.02 * (t89Count + abCount + panzerCount + tokuHoni1Count)
 						+ 0.03 * armedCount
 						+ 0.01 * t2Count;
@@ -466,7 +470,7 @@ Contains summary information about a fleet and its ships
 			[0.054, 0.056, 0.058, 0.059][normalCount] || 0.06 :
 			[0.050, 0.050, 0.052, 0.054][normalCount] || 0.054;
 		const tokuBonus = Math.min(tokuCap, 0.02 * tokuCount);
-		const landingCraftCount = normalCount + t89Count + t2Count + tokuCount + abCount + armedCount + panzerCount + tokuHoni1Count;
+		const landingCraftCount = normalCount + t89Count + t2Count + t4Count + tokuCount + abCount + armedCount + panzerCount + tokuHoni1Count;
 		// "Bstar" in the formula
 		const improveBonus = landingCraftCount > 0
 			? 0.01 * improveCount * cappedBasicBonus / landingCraftCount
--- src/library/objects/Ship.js
+++ src/library/objects/Ship.js
@@ -1918,16 +1918,16 @@ KC3改 Ship Object
 	 */
 	KC3Ship.prototype.calcLandingCraftBonus = function(installationType = 0, isNight = false){
 		if(this.isDummy() || ![1, 2, 3, 4, 5].includes(installationType)) { return 0; }
-		// 8 types of (17 gears) Daihatsu Landing Craft with known bonus:
-		//  * 0: [167,525,526] Special Type 2 Amphibious Tank, T4 Tank, T4 Tank Kai, the real 'Tank' type
+		// 8 types of (17 gears) Daihatsu Landing Craft with known bonus, 2 unknown:
+		//  * 0: [167,525,526] Special Type 2 Amphibious Tank, T4 Tank?, T4 Tank Kai?, the real 'Tank' type
 		//  * 1: [166,449,494,495,482,514] Daihatsu Landing Craft (Type 89 Medium Tank & Landing Force), Toku Daihatsu Landing Craft + Type 1 Gun Tank, Toku Daihatsu Landing Craft + Chi-Ha (conditional, Kai either), Panzer III
 		//  * 2: [68] Daihatsu Landing Craft
 		//  * 3: [230] Toku Daihatsu Landing Craft + 11th Tank Regiment
-		//  * 4: [193,482,514] Toku Daihatsu Landing Craft, Toku Daihatsu Landing Craft + Panzer III (North African Specification), Toku Daihatsu Landing Craft + Panzer III Ausf. J?
-		//  * 5: [355,495,514] M4A1 DD, Toku Daihatsu Landing Craft + Chi-Ha Kai, Panzer III Ausf. J?
+		//  * 4: [193,482,514] Toku Daihatsu Landing Craft, Toku Daihatsu Landing Craft + Panzer III (North African Specification), Toku Daihatsu Landing Craft + Panzer III Ausf. J
+		//  * 5: [355,495,514] M4A1 DD, Toku Daihatsu Landing Craft + Chi-Ha Kai, Panzer III Ausf. J
 		//  * 6: [408,409] Soukoutei (Armored Boat Class), Armed Daihatsu
 		//  * 7: [436] Daihatsu Landing Craft (Panzer II / North African Specification)
-		const landingCraftIds = [[167, 525, 526], [166, 449, 494, 495, 482, 514], 68, 230, [193, 482, 514], [355, 495, 514], [408, 409], 436];
+		const landingCraftIds = [[167], [166, 449, 494, 495, 482, 514], 68, 230, [193, 482, 514], [355, 495, 514], [408, 409], 436];
 		const landingCraftCounts = landingCraftIds.map(id => this.countEquipment(id));
 		const landingModifiers = KC3GearManager.landingCraftModifiers[installationType - 1] || {};
 		const getModifier = (type, modName = "base") => (
