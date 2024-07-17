c1d3d336ec72e5d9ef78f9a3d549f1c40a1d5868
sin_sin <sinsinpub@gmail.com>
2024-07-17 21:30:35 +0800

    Add summer 2024 event tab
    * Update TL pointer

--- src/library/modules/Meta.js
+++ src/library/modules/Meta.js
@@ -1414,7 +1414,7 @@ Provides access to data on built-in JSON files
 						seasons : ["Winter", "Spring", "Summer", "Fall"],
 						fromId : 21,
 						fromYear : 2013,
-						skippedSeasons : [[42, 2], [48, 3], [50, 4], [56, 6], [58, 7]],
+						skippedSeasons : [[42, 2], [48, 3], [50, 4], [56, 6], [58, 7], [59, 8]],
 					},
 					period = eventMapDefs.seasons.length,
 					worldIndex = eventMapDefs.skippedSeasons.reduce((index, [skipFrom, skipAccumulated]) => (
