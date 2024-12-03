702b9ca1bff8c383496c492f2fc612fc0220e7e0
sin_sin <sinsinpub@gmail.com>
2024-12-03 23:02:42 +0800

    WiP updates for maint:
    * Update manifest and server info
      * Update to new server hostnames in domains instead of ip addresses
    * Update remodel info
    * Update phase2 item icons
    * Update TL pointer

--- src/library/objects/Server.js
+++ src/library/objects/Server.js
@@ -6,7 +6,9 @@ Represent KanColle game server instance.
 (function(){
 	"use strict";
 	
-	window.KC3Server = function(num, isDomain){
+	const kcBaseDomain = ".kancolle-server.com";
+	
+	window.KC3Server = function(num, isSecured){
 		this.num = 0;
 		this.ip = "";
 		this.name = "";
@@ -14,16 +16,18 @@ Represent KanColle game server instance.
 		this.protocol = "";
 		this.urlPrefix = "";
 		this.isDomain = false;
-		if(num) this.setNum(num, isDomain);
+		if(num) this.setNum(num, isSecured);
 	};
 	
 	KC3Server.prototype.setUrl = function(url){
 		const anchor = document.createElement("a");
 		anchor.href = url;
 		this.ip = anchor.host;
-		// kc still using http and ip addresses, fake sub domain reserved for future
+		// kc still using http and ip addresses (partially)
+		// gadget and w01 (yokosuka) server have started to use domain since 2024-12-03
+		// eg: w00g.       w01y.     kancolle-server.com
 		this.protocol = anchor.protocol;
-		const domainMatch = (this.ip || "").match(/world(\d+)\.kan-colle\.com/);
+		const domainMatch = (this.ip || "").match(/w(\d+)\w+\.kancolle-server\.com/);
 		this.isDomain = !!(domainMatch && domainMatch[1]);
 		const serverInfo = this.isDomain ? KC3Meta.serverByNum(domainMatch[1]) : KC3Meta.server(this.ip);
 		this.num = serverInfo.num;
@@ -33,21 +37,21 @@ Represent KanColle game server instance.
 			this.ip = serverInfo.ip;
 			this.urlPrefix = `${this.protocol}//${this.host}`;
 		} else {
-			this.host = `world${this.num}.kan-colle.com`;
+			this.host = serverInfo.domain || `w${String(this.num).pad(2,"0")}y` + kcBaseDomain;
 			this.urlPrefix = `${this.protocol}//${this.ip}`;
 		}
 		return this;
 	};
 	
-	KC3Server.prototype.setNum = function(num, isDomain = false){
+	KC3Server.prototype.setNum = function(num, isSecured = false){
 		const serverInfo = KC3Meta.serverByNum(num);
 		this.num = serverInfo.num;
 		this.ip = serverInfo.ip;
 		this.name = serverInfo.name;
-		// update this if real domain used in future
-		this.host = `world${this.num}.kan-colle.com`;
-		this.protocol = isDomain ? "https:" : "http:";
-		this.urlPrefix = `${this.protocol}//${isDomain ? this.host : this.ip}`;
+		this.host = serverInfo.domain || `w${String(this.num).pad(2,"0")}y` + kcBaseDomain;
+		this.isDomain = !!serverInfo.domain;
+		this.protocol = isSecured ? "https:" : "http:";
+		this.urlPrefix = `${this.protocol}//${this.isDomain ? this.host : this.ip}`;
 		return this;
 	};
 	
