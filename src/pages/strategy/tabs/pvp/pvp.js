2fec2b9ec3554a2a041a616c125e8c6ad40a7c42
sin_sin <sinsinpub@gmail.com>
2025-04-13 14:12:59 +0800

    Fallback to jquery impl to do http fetch
    * Also allow meta key, updated helps

--- src/pages/strategy/tabs/pvp/pvp.js
+++ src/pages/strategy/tabs/pvp/pvp.js
@@ -410,25 +410,40 @@
 						self.exportingReplay = false;
 						$("body").css("opacity", "1");
 						return true;
-					} else if(e.ctrlKey) {
-						fetch('https://kcrdb.hitomaru.dev/replays', {
-							method: 'POST',
+					} else if(e.ctrlKey || e.metaKey) {
+						$.ajax({
+							url: "https://kcrdb.hitomaru.dev/replays",
+							async: true,
+							method: "POST",
 							headers: {
-								'Accept': 'application/json',
-								'Content-Type': 'application/json'
+								"Accept": "application/json",
+								"Content-Type": "application/json",
+								"Data-Origin": "KC3"
 							},
-							body: JSON.stringify({ data: encodeData }),
-						}).then(async(res) => {
-							if (res.ok) {
-								let data = await res.json();
-								let url = 'https://kc3kai.github.io/kancolle-replay/?r=' + data.id;
+							data: JSON.stringify({ data: encodeData }),
+							complete: (xhr) => {
+								self.exportingReplay = false;
+								$("body").css("opacity", "1");
+							},
+							success: (data) => {
+								console.debug("KCRDB returned data", data);
+								const url = "https://kc3kai.github.io/kancolle-replay/?r=" + data.id;
 								self.copyToClipboard(url, () => {
 									alert(KC3Meta.term("BattleReplayToClipboard"));
 								});
-							}
+							},
+							error: (xhr, statusText, httpError) => {
+								let msg = httpError || [statusText, xhr.status].filter(v => !!v).join(" ") || "Error";
+								const json = xhr.responseJSON;
+								if(json) {
+									msg = [[json.statusCode, json.error].join(" ")];
+									if(Array.isArray(json.message)) msg = msg.concat(json.message); else msg.push(json.message);
+									msg = msg.join("\n");
+								}
+								console.debug(["HTTP", statusText, xhr.status].join(" "), json);
+								alert(msg);
+							},
 						});
-						self.exportingReplay = false;
-						$("body").css("opacity", "1");
 						return true;
 					}
 					
