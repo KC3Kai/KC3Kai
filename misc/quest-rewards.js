523a79e0ceb997b7f033fcaed380fbe05a550c5a
sin_sin <sinsinpub@gmail.com>
2025-12-04 19:18:26 +0800

    35.6.47
    * Update TL pointer

--- misc/quest-rewards.js
+++ misc/quest-rewards.js
@@ -10,13 +10,17 @@ copy(JSON.stringify(rewards,null,"\t"))
 
 // check id diff for new quests
 diff=quests.items.filter(q => (!KC3Meta.quest(q.api_no))); meta={}; diff.forEach(q => { meta[q.api_no]={ code:"???", name:q.api_title, desc:q.api_detail, memo:"※", hash:q.api_title.hashCode()}; }); console.debug(diff.length, meta);
-copy(JSON.stringify(meta,null,"    "))
+copy(meta)
 
 // check text diff for correcting jp json
 fetch("https://kcrdb.hitomaru.dev/quests/data?is_mod=false&is_verified=false").then(resp => (resp.ok && resp.json())).then(o => { quests=o; console.debug(quests); });
 diff=quests.items.filter(q => { m = KC3Meta.quest(q.api_no); return m.name != q.api_title || m.desc.replace(/<br>/g,'') != q.api_detail.replace(/<br>/g,''); }).map(q => ({ id: q.api_no, name: q.api_title, desc: q.api_detail, difftitle: q.api_title != KC3Meta.quest(q.api_no).name })); console.debug(diff);
-copy(JSON.stringify(diff,null,"    "))
+copy(diff)
+
+// find if still dupe/reuse quest ids
+fetch("https://kcrdb.hitomaru.dev/quests/data?is_mod=false&has_api_select_rewards=false&sort=api_no").then(resp => (resp.ok && resp.json())).then(o => { quests=o; qids=o.items.map(q => q.api_no); rids=qids.filter((n,i) => i>0 && n==qids[i-1]); console.debug(quests.total, rids); });
+copy(JSON.stringify(rids))
 
 // update to tl json temporarily for tests
-meta=KC3Meta._quests; quests.items.forEach(q => { m=meta[q.api_no]||{}; m.name=q.api_title; m.desc=q.api_detail; }); console.debug(meta); copy(JSON.stringify(meta,null,"    "))
+meta=KC3Meta._quests; quests.items.forEach(q => { m=meta[q.api_no]||{}; m.name=q.api_title; m.desc=q.api_detail; }); console.debug(meta); copy(meta)
 
