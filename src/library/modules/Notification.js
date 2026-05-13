c141710fcfcc33c3b4441354ad9666c1e3f17748
sin_sin <sinsinpub@gmail.com>
2026-05-13 12:36:45 +0800

    Enclose food supply icon with circle
    * Update notification module names and comments

--- src/library/modules/Notification.js
+++ src/library/modules/Notification.js
@@ -1,9 +1,14 @@
+/**
+ * KC3改 configured Chrome Desktop Notifications sender triggered from devtools panel events.
+ * @see Timer.js - another notifications sender triggered by managed timer events.
+ * @see Service.js - background messaging service to invoke `chrome.notifications` API actually.
+ */
 (() => {
+  'use strict';
 
-  let audio = new Audio();
-  audio = null;
+  var audio = null;
 
-  function clear() {
+  function clearSound() {
     if (!audio) {
       return;
     }
@@ -12,66 +17,32 @@
     audio = null;
   }
 
-  function getSrc(alertType, alertCustomSrc) {
+  function getSoundSrc(alertType, alertCustomSrc) {
     switch (alertType) {
       case 1:
-        return '../../../../assets/snd/pop.mp3';
+        return '/assets/snd/pop.mp3';
       case 2:
         return alertCustomSrc;
       case 3:
-        return '../../../../assets/snd/ding.mp3';
+        return '/assets/snd/ding.mp3';
       case 4:
-        return '../../../../assets/snd/dong.mp3';
+        return '/assets/snd/dong.mp3';
       case 5:
-        return '../../../../assets/snd/bell.mp3';
+        return '/assets/snd/bell.mp3';
     }
-    return null;
+    return;
   }
 
-  function notify(alertType, alertCustomSrc) {
-    clear();
-
-    const src = getSrc(alertType, alertCustomSrc);
+  function playSound(alertType, alertCustomSrc) {
+    const src = getSoundSrc(alertType, alertCustomSrc);
     if (!src) {
       return;
     }
 
+    clearSound();
     audio = new Audio(src);
     audio.volume = ConfigManager.alert_volume / 100;
     audio.play();
-  }
-
-  function notifyMorale() {
-    const canNotify = !!ConfigManager.alert_morale_notif
-      && !(KC3SortieManager.isOnSortie() || KC3SortieManager.isPvP());
-    if (!canNotify) {
-      return;
-    }
-
-    notify(ConfigManager.alert_type, ConfigManager.alert_custom);
-    notifyDesktop('morale', {
-      type: 'basic',
-      title: KC3Meta.term('DesktopNotifyMoraleTitle'),
-      message: KC3Meta.term('DesktopNotifyMoraleMessage'),
-      iconUrl: '/assets/img/ui/morale.png'
-    });
-    focusTab();
-  }
-
-  function notifySparkle() {
-    const canNotify = !!ConfigManager.alert_sparkle;
-    if (!canNotify) {
-      return;
-    }
-
-    notify(ConfigManager.alert_type_sparkle, ConfigManager.alert_custom_sparkle);
-    notifyDesktop('sparkle', {
-      type: 'basic',
-      title: KC3Meta.term('DesktopNotifySparkleTitle'),
-      message: KC3Meta.term('DesktopNotifySparkleMessage'),
-      iconUrl: '/assets/img/ui/foodsup.png'
-    });
-    focusTab();
   }
 
   function notifyDesktop(notifId, data) {
@@ -92,13 +63,47 @@
     })).execute();
   }
 
+  /** On event of ship morale recovered to average */
+  function notifyMorale() {
+    const canNotify = !!ConfigManager.alert_morale_notif
+      && !(KC3SortieManager.isOnSortie() || KC3SortieManager.isPvP());
+    if (!canNotify) {
+      return;
+    }
+
+    playSound(ConfigManager.alert_type, ConfigManager.alert_custom);
+    notifyDesktop('morale', {
+      type: 'basic',
+      title: KC3Meta.term('DesktopNotifyMoraleTitle'),
+      message: KC3Meta.term('DesktopNotifyMoraleMessage'),
+      iconUrl: '/assets/img/ui/morale.png'
+    });
+    focusTab();
+  }
+
+  /** On event of ship morale increased above average (sparkled state) */
+  function notifySparkle() {
+    const canNotify = !!ConfigManager.alert_sparkle;
+    if (!canNotify) {
+      return;
+    }
+
+    playSound(ConfigManager.alert_type_sparkle, ConfigManager.alert_custom_sparkle);
+    notifyDesktop('sparkle', {
+      type: 'basic',
+      title: KC3Meta.term('DesktopNotifySparkleTitle'),
+      message: KC3Meta.term('DesktopNotifySparkleMessage'),
+      iconUrl: '/assets/img/ui/foodsup.png'
+    });
+    focusTab();
+  }
+
   window.KC3Notification = {
-    clear,
-    notify,
+    playSound,
+    focusTab,
+    notifyDesktop,
     notifyMorale,
     notifySparkle,
-    notifyDesktop,
-    focusTab,
   };
 
 })();
